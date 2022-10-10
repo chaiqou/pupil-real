4481615f9cc6d00d5731ce9fb37ee7a906ddec2c
chaiqou <lomtadzenikusha@gmail.com>
2022-10-10 21:39:41 +0400

    chore: remove unnecessary cdn from blade

--- resources/views/sign-in.blade.php
+++ resources/views/sign-in.blade.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html>
-<html class="h-full bg-gray-50" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html class="h-full bg-gray-50">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>Sign in | PupilPay</title>
-
-    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
     <link rel="stylesheet" href="<?php echo asset('css/app.css'); ?>" type="text/css">
 </head>
 
