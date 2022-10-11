c8970b0c39116b5098a1d00cc0d9708a6768e3d2
chaiqou <lomtadzenikusha@gmail.com>
2022-10-11 19:19:17 +0400

    style: update reset password form page

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -9,34 +9,36 @@
 </head>
 
 <body class="h-full">
-    <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8 ">
         <div class="w-full max-w-md space-y-8">
             <div>
                 <img class="mx-auto h-12 w-auto" src="https://pupilpay.hu/resc/img/pupilpay-black-color.svg"
                     alt="PupilPay" />
-                <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Reset your password
-                </h2>
+                <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Reset your password</h2>
             </div>
             <form class="mt-8 space-y-6" action="{{ route('password.update') }}" method="POST">
                 @csrf
+                <input type="hidden" name="remember" value="true" />
                 <input type="hidden" name="token" value="{{ $token }}">
-                <input type="hidden" id="email" name="email" autofocus value="{{ $email }}" <div
-                    class="-space-y-px rounded-md shadow-sm">
-                <div>
-                    <label for="password" class="sr-only">New password</label>
-                    <input id="password" name="password" type="password" autocomplete="password"
-                        class="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                        placeholder="New password" />
+                <input type="hidden" id="email" name="email" autofocus value="{{ $email }}">
+                <div class="-space-y-px rounded-md shadow-sm">
+                    <div>
+                        <label for="password" class="sr-only">Email address</label>
+                        <input id="password" name="password" type="password" autocomplete="password"
+                            class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            placeholder="New password" />
+                    </div>
+                    <div>
+                        <label for="password_confirmation" class="sr-only">Password</label>
+                        <input id="password_confirmation" name="password_confirmation" type="password"
+                            autocomplete="password_confirmation"
+                            class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            placeholder="Repeat new password" />
+                    </div>
+
                 </div>
-                <div>
-                    <label for="password_confirmation" class="sr-only">Password</label>
-                    <input id="password_confirmation" name="password_confirmation" type="password"
-                        autocomplete="password_confirmation"
-                        class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                        placeholder="Repeat new password" />
-                </div>
-                @error('email')
-                    <div class="rounded-md bg-red-50 p-4 mt-3">
+                @error('password')
+                    <div class="rounded-md bg-red-50 p-4">
                         <div class="flex">
                             <div class="flex-shrink-0">
                                 <svg class="h-5 w-5 text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
@@ -52,25 +54,24 @@
                         </div>
                     </div>
                 @enderror
-        </div>
 
-        <div>
-            <button type="submit"
-                class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-                    <!-- Heroicon name: mini/key -->
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                        class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400">
-                        <path fill-rule="evenodd"
-                            d="M8 7a5 5 0 113.61 4.804l-1.903 1.903A1 1 0 019 14H8v1a1 1 0 01-1 1H6v1a1 1 0 01-1 1H3a1 1 0 01-1-1v-2a1 1 0 01.293-.707L8.196 8.39A5.002 5.002 0 018 7zm5-3a.75.75 0 000 1.5A1.5 1.5 0 0114.5 7 .75.75 0 0016 7a3 3 0 00-3-3z"
-                            clip-rule="evenodd" />
-                    </svg>
-                </span>
-                Set new password
-            </button>
+                <div>
+                    <button type="submit"
+                        class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                        <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+                            <!-- Heroicon name: mini/key -->
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400">
+                                <path fill-rule="evenodd"
+                                    d="M8 7a5 5 0 113.61 4.804l-1.903 1.903A1 1 0 019 14H8v1a1 1 0 01-1 1H6v1a1 1 0 01-1 1H3a1 1 0 01-1-1v-2a1 1 0 01.293-.707L8.196 8.39A5.002 5.002 0 018 7zm5-3a.75.75 0 000 1.5A1.5 1.5 0 0114.5 7 .75.75 0 0016 7a3 3 0 00-3-3z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </span>
+                        Set new password
+                    </button>
+                </div>
+            </form>
         </div>
-        </form>
-    </div>
     </div>
 </body>
 
