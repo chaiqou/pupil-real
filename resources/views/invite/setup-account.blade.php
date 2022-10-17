ceb82a1542b5dfea1b41297a3470d7961b29af6a
Leventdev <kazo.levente@gmail.com>
2022-10-17 20:43:07 +0200

    Update account setup

--- resources/views/invite/setup-account.blade.php
+++ resources/views/invite/setup-account.blade.php
@@ -1,78 +1,209 @@
+<!DOCTYPE html>
+<html class="h-full bg-gray-50">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="<?php echo asset('css/app.css') ?>" type="text/css">
+    <title>Set up account | PupilPay</title>
+    <link rel="stylesheet" href="<?php echo asset('css/app.css'); ?>" type="text/css">
 </head>
 
-<body>
+<body class="h-full">
+    <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div class="w-full max-w-md space-y-8" data-sal="slide-up" data-sal-duration="500" data-sal-delay="200">
+            <nav aria-label="Progress">
+                <ol role="list" class="divide-y divide-gray-300 rounded-md border border-gray-300 md:flex md:divide-y-0 md:w-fit">
+                    <li class="relative md:flex md:flex-1">
+                        <!-- Completed Step -->
+                        <div class="group flex w-full items-center">
+                            <span class="flex items-center px-6 py-4 text-sm font-medium">
+                                <span class="flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full border-2 border-indigo-600">
+                                    <span class="text-indigo-600">01</span>
+                                </span>
+                                <span class="ml-4 text-sm font-medium text-indigo-600">Set up Account</span>
+                            </span>
+                        </div>
+                    </li>
 
-<main class="flex justify-center mt-20 md:mt-48 flex-col items-center">
+                    <li class="relative md:flex md:flex-1">
+                        <!-- Current Step -->
+                        <div class="flex items-center px-6 py-4 text-sm font-medium" aria-current="step">
+                            <span class="flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full border-2 border-gray-300">
+                                <span class="text-gray-500">02</span>
+                            </span>
+                            <span class="ml-4 text-sm font-medium text-gray-500">Personal Form</span>
+                        </div>
 
+                    </li>
 
-
-
-   <div class="border-[0.1rem] text-gray-400 font-bold text-xl border-gray-300 px-4 py-4 rounded-md hidden md:flex">
-       <article class="flex items-center text-[#3341FF]">
-           <div class="mr-3 p-5 rounded-full border-2 border-[#3341FF] w-4 h-4 justify-center items-center flex">
-               <p>01</p>
-           </div>
-           <div>
-               <p>Setup Account</p>
-           </div>
-       </article>
-       <article class="flex mx-16 items-center">
-           <div class="mr-3 p-5 rounded-full border-2 border-gray-400 w-4 h-4 justify-center items-center flex">
-               <p>02</p>
-           </div>
-           <div>
-               <p>Personal Form</p>
-           </div>
-       </article>
-       <article class="flex items-center">
-           <div class="mr-3 p-5 rounded-full border-2 border-gray-400 w-4 h-4 justify-center items-center flex">
-               <p>03</p>
-           </div>
-           <div>
-               <p>Verify Account</p>
-           </div>
-       </article>
-   </div>
-
-        <div class="flex md:mt-12 flex-col items-center ">
-            <div class="w-60 md:w-80">
-                <img src="{{asset('/img/pupilpay-black-color.svg')}}" alt="pupilpay-black-color"/>
-            </div>
-            <h1 class="text-3xl md:text-4xl font-extrabold mt-7">Set up your account</h1>
-        </div>
-
-    <div class="mt-12 border-t-[1px] border-b-[3px] border-l-[1.5px] border-r-[1.5px]  border-opacity-10 shadow-md border-gray-400 p-7 mb-12 rounded-md">
-        <form id="form" method="POST" action="{{route('setup.account_submit', ['uniqueID' => $uniqueID])}}" class="flex flex-col">
-            @csrf
+                    <li class="relative md:flex md:flex-1">
+                        <!-- Upcoming Step -->
+                        <div class="group flex items-center">
+                            <span class="flex items-center px-6 py-4 text-sm font-medium">
+                                <span class="flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full border-2 border-gray-300">
+                                    <span class="text-gray-500">03</span>
+                                </span>
+                                <span class="ml-4 text-sm font-medium text-gray-500">Verify Account</span>
+                            </span>
+                        </div>
+                    </li>
+                </ol>
+            </nav>
             <div>
-                <label for="email">Email Address</label>
-                <div class="p-2 mt-2 bg-gray-50 rounded-md border-gray-400 border-[0.01rem]">
-                    <input id="email" value="{{old('email')}}" name="email" type="text" class="border-none focus:ring-0 bg-gray-50 w-[15rem] md:w-[44.7rem]"/>
+                <img class="mx-auto h-16 w-auto" src="<?php echo asset('img/pupilpay-black-color.svg') ?>" alt="PupilPay" />
+                <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Set up your account</h2>
+            </div>
+            @if (session()->has('warning'))
+            <div class="rounded-md bg-yellow-50 p-4">
+                <div class="flex">
+                    <div class="flex-shrink-0">
+                        <!-- Heroicon name: mini/exclamation-triangle -->
+                        <svg class="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                            <path fill-rule="evenodd" d="M8.485 3.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 3.495zM10 6a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 6zm0 9a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                    <div class="ml-3">
+                        <h3 class="text-sm font-medium text-yellow-800">{{ session()->get('warning_title') }}</h3>
+                        <div class="mt-2 text-sm text-yellow-700">
+                            <p>{{ session()->get('warning_message') }}</p>
+                        </div>
+                    </div>
                 </div>
-                @error('email')
-                <p class="mt-2 text-red-500">{{$message}}</p>
+            </div>
+            @endif
+            @error('password')
+                    <div class="rounded-md bg-red-50 p-4">
+                        <div class="flex">
+                            <div class="flex-shrink-0">
+                                <svg class="h-5 w-5 text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                    fill="currentColor" aria-hidden="true">
+                                    <path fill-rule="evenodd"
+                                        d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                            <div class="ml-3">
+                                <h3 class="text-sm font-medium text-red-800">{{ $message }}</h3>
+                            </div>
+                        </div>
+                    </div>
                 @enderror
+            @error('email')
+                    <div class="rounded-md bg-red-50 p-4">
+                        <div class="flex">
+                            <div class="flex-shrink-0">
+                                <svg class="h-5 w-5 text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                    fill="currentColor" aria-hidden="true">
+                                    <path fill-rule="evenodd"
+                                        d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                            <div class="ml-3">
+                                <h3 class="text-sm font-medium text-red-800">{{ $message }}</h3>
+                            </div>
+                        </div>
+                    </div>
+                @enderror
+            @if (session()->has('error'))
+            <div class="rounded-md bg-red-50 p-4">
+                <div class="flex">
+                    <div class="flex-shrink-0">
+                        <!-- Heroicon name: mini/exclamation-circle -->
+                        <svg class="h-5 w-5 text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                            <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-5a.75.75 0 01.75.75v4.5a.75.75 0 01-1.5 0v-4.5A.75.75 0 0110 5zm0 10a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                    <div class="ml-3">
+                        <h3 class="text-sm font-medium text-red-800">
+                            @if (session()->has('error_title'))
+                            {{ session()->get('error_title') }}
+                            @else
+                            Authentication failed
+                            @endif
+                        </h3>
+                        <div class="mt-2 text-sm text-red-700">
+                            <p>
+                                @if (session()->has('error_message'))
+                                {{ session()->get('error_message') }}
+                                @else
+                                The email address or password you entered is incorrect.
+                                @endif
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @endif
+            @if (session()->has('success'))
+            <div class="rounded-md bg-green-50 p-4">
+                <div class="flex">
+                    <div class="flex-shrink-0">
+                        <!-- Heroicon name: mini/check-circle -->
+                        <svg class="h-5 w-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                    <div class="ml-3">
+                        <h3 class="text-sm font-medium text-green-800">{{ session()->get('success_title') }}</h3>
+                        <div class="mt-2 text-sm text-green-700">
+                            <p>{{ session()->get('success_message') }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @endif
+            <form id="form" method="POST" action="{{route('setup.account_submit',['uniqueID'=>$uniqueID])}}" class="mt-8 space-y-6">
+                @csrf
+                <input type="hidden" name="remember" value="true"/>
+                <div class="-space-y-px rounded-md shadow-sm">
+                    <div>
+                        <label for="email" class="sr-only">Email address</label>
+                        <input id="email-address" name="email" type="email" autocomplete="email" required
+                        value="{{ $invite->email}}"
+                            class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            placeholder="Email address" />
+                    </div>
+                    <div class="flex">
+                        <div class="w-full">
+                            <label for="password" class="sr-only">Password</label>
+                            <input id="password" name="password" type="password" autocomplete="new-password" required class="relative block w-full appearance-none rounded-none rounded-bl-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Password" />
+                        </div>
+                        <button onmouseover="document.getElementById('password').type='text'" onmouseleave="document.getElementById('password').type='password'" type="button" class="relative -ml-px inline-flex items-center space-x-2 rounded-br-md border border-gray-300 bg-gray-50 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500">
+                            <!-- Heroicon name: mini/eye -->
+                            <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <path d="M10 12.5a2.5 2.5 0 100-5 2.5 2.5 0 000 5z" />
+                                <path fill-rule="evenodd" d="M.664 10.59a1.651 1.651 0 010-1.186A10.004 10.004 0 0110 3c4.257 0 7.893 2.66 9.336 6.41.147.381.146.804 0 1.186A10.004 10.004 0 0110 17c-4.257 0-7.893-2.66-9.336-6.41zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+                <div>
+                    <button type="submit"
+                    id="submit"
+                        class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                        <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+                            <!-- Heroicon name: mini/user-circle -->
+                            <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                aria-hidden="true">
+                                <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-5.5-2.5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zM10 12a5.99 5.99 0 00-4.793 2.39A6.483 6.483 0 0010 16.5a6.483 6.483 0 004.793-2.11A5.99 5.99 0 0010 12z" clip-rule="evenodd" />
+                            </svg>
 
-            </div>
-            <div class="mt-4">
-                <label for="password">Password</label>
-                <div class="p-2 mt-2 bg-gray-50 rounded-md border-gray-400 border-[0.01rem]">
-                    <input id="password" name="password" type="password" class="border-none focus:ring-0 bg-gray-50 w-[15rem] md:w-[44.7rem]"/>
+                        </span>
+                        Sign up
+                    </button>
                 </div>
-                @error('password')
-                <p class="mt-2 text-red-500">{{$message}}</p>
-                @enderror
-            </div>
-            <button type="submit" class="bg-[#3341FF] text-white p-3 mt-8 text-xl rounded-md">Sign up</button>
-        </form>
+            </form>
+        </div>
     </div>
-
-</main>
-
+    <script src="<?php echo asset('js/sal.js') ?>"></script>
+    <script>
+        sal({
+            threshold: 0,
+            once: true,
+        });
+    </script>
 </body>
 
-
+</html>
