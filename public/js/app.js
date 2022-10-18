098beccdfeea4ce36d741d3f70488bada31a288c
pickle99 <jackrestler@gmail.com>
2022-10-18 15:54:56 +0400

    fix: change ewindow to window in bootstrap.js

--- public/js/app.js
+++ public/js/app.js
@@ -37095,12 +37095,12 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 /* harmony import */ var _SetupAccount_vue_vue_type_template_id_fbf1e64a__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./SetupAccount.vue?vue&type=template&id=fbf1e64a */ "./resources/js/components/SetupAccount.vue?vue&type=template&id=fbf1e64a");
-/* harmony import */ var _home_nika_Desktop_Portal_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./node_modules/vue-loader/dist/exportHelper.js */ "./node_modules/vue-loader/dist/exportHelper.js");
+/* harmony import */ var _home_xjexe_Portal_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./node_modules/vue-loader/dist/exportHelper.js */ "./node_modules/vue-loader/dist/exportHelper.js");
 
 const script = {}
 
 ;
-const __exports__ = /*#__PURE__*/(0,_home_nika_Desktop_Portal_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_1__["default"])(script, [['render',_SetupAccount_vue_vue_type_template_id_fbf1e64a__WEBPACK_IMPORTED_MODULE_0__.render],['__file',"resources/js/components/SetupAccount.vue"]])
+const __exports__ = /*#__PURE__*/(0,_home_xjexe_Portal_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_1__["default"])(script, [['render',_SetupAccount_vue_vue_type_template_id_fbf1e64a__WEBPACK_IMPORTED_MODULE_0__.render],['__file',"resources/js/components/SetupAccount.vue"]])
 /* hot reload */
 if (false) {}
 
