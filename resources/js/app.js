15585d58d67bb8ed84dfa2bc9ec8460bdf5cc19c
chaiqou <lomtadzenikusha@gmail.com>
2022-10-20 20:39:27 +0400

    wip(style): create select students card

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,7 @@ import DashboardNavigation from './components/DashboardNavigation.vue'
 import DashboardNavigationMobile from './components/DashboardNavigationMobile.vue'
 import NavigationMenuButton from './components/NavigationMenuButton.vue'
 import SwitchAccount from './components/SwitchAccount.vue'
+import SelectStudents from './components/SelectStudents.vue'
 
 const pinia = createPinia()
 const app = createApp({})
@@ -15,6 +16,7 @@ app.component('dashboard-navigation', DashboardNavigation)
 app.component('dashboard-navigation-mobile', DashboardNavigationMobile)
 app.component('navigation-menu-button', NavigationMenuButton)
 app.component('switch-account', SwitchAccount)
+app.component('select-students', SelectStudents)
 
 app.use(pinia)
 app.mount('#app')
