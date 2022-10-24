6e18e1457a77698a968a3e4269ebb695c137d37a
chaiqou <lomtadzenikusha@gmail.com>
2022-10-24 15:56:55 +0400

    chore: group my defined middlewares

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,9 @@ use App\Http\Controllers\Auth\ForgotPasswordController;
 use App\Http\Controllers\Auth\TwoFactorAuthenticationController;
 
 
-Route::get('/', [AuthController::class, 'redirectIfLoggedIn'])->middleware('guest')->name('default');
-Route::post('/login', [AuthController::class, 'authenticate'])->middleware('guest')->name('login');
-
 Route::middleware(['guest'])->group(function () {
+    Route::get('/', [AuthController::class, 'redirectIfLoggedIn'])->name('default');
+    Route::post('/login', [AuthController::class, 'authenticate'])->name('login');
 	Route::get('/forgot-password', [ForgotPasswordController::class, 'forgotPasswordForm'])->name('forgot.form');
 	Route::post('/forgot-password', [ForgotPasswordController::class, 'sendForgotPasswordMail'])->name('forgot.request');
 	Route::get('/mail-sent', [ForgotPasswordController::class, 'forgotRedirect'])->name('forgot.redirect');
@@ -21,10 +20,16 @@ Route::middleware(['guest'])->group(function () {
 	Route::post('/reset-password', [ResetPasswordController::class, 'passwordUpdate'])->name('password.update');
 });
 
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::get('/parent/dashboard', [ParentController::class, 'parentDashboard'])->name('parents.dashboard');
-Route::get('/parent/dashboard/{student_id}', [NavigationController::class, 'index'])->middleware('auth')->name('dashboard');
+Route::middleware(['auth'])->group(function(){
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/parent/dashboard', [ParentController::class, 'parentDashboard'])->name('parents.dashboard');
+    Route::get('/parent/dashboard/{student_id}', [NavigationController::class, 'index'])->name('dashboard');
+    Route::get('/two-factor-authentication', [TwoFactorAuthenticationController::class, 'form'])->name('2fa.form');
+    Route::post('/submit-two-factor-authentication', [TwoFactorAuthenticationController::class, 'verify'])->name('2fa.submit');
+    Route::post('/resend-two-factor-authentication', [TwoFactorAuthenticationController::class, 'resend'])->name('2fa.resend');
+});
+
 
 Route::get('/send-invite', [InviteController::class, 'index'])->name('invite.user');
 Route::post('/send-invite', [InviteController::class, 'sendInvite'])->name('send.invite');
@@ -34,11 +39,6 @@ Route::post('/setup-account/{uniqueID}', [InviteController::class, 'submitSetupA
 
 Route::get('/personal-form/{uniqueID}', [InviteController::class, 'personalForm'])->name('personal.form');
 Route::post('/personal-form/{uniqueID}', [InviteController::class, 'submitPersonalForm'])->name('personal.form_submit');
-
-Route::get('/two-factor-authentication', [TwoFactorAuthenticationController::class, 'form'])->name('2fa.form');
-Route::post('/submit-two-factor-authentication', [TwoFactorAuthenticationController::class, 'verify'])->name('2fa.submit');
-Route::post('/resend-two-factor-authentication', [TwoFactorAuthenticationController::class, 'resend'])->name('2fa.resend');
-
 
 Route::get('/verify-email/{uniqueID}', [InviteController::class, 'verifyEmail'])->name('verify.email');
 Route::post('/verify-email/{uniqueID}', [InviteController::class, 'submitVerifyEmail'])->name('verify.email_submit');
