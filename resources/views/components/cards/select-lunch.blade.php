753bbdaa5ebc6c4d201644f5fb6ee37f772ffd64
chaiqou <lomtadzenikusha@gmail.com>
2022-10-25 14:05:35 +0400

    style: create select lunch style

---
+++ resources/views/components/cards/select-lunch.blade.php
@@ -0,0 +1,22 @@
+<div class='w-full'>
+    <ul role="list" class="space-y-4 w-full sm:grid sm:grid-cols-2 sm:gap-6 sm:space-y-0 lg:grid-cols-3 lg:gap-8">
+        <li class="rounded-lg bg-gray-500 py-10 px-6 text-center xl:px-10 xl:text-left">
+            <a href="#" class="group flex flex-col items-center gap-2">
+                <span class="inline-flex h-20 w-20 items-center justify-center rounded-full bg-gray-200">
+                    <span class="text-xl font-medium leading-none text-white">ICON</span>
+                </span>
+                <p class="text-red-500 mt-2">
+                    Name</p>
+            </a>
+        </li>
+        <li class="rounded-lg bg-gray-500 py-10 px-6 text-center xl:px-10 xl:text-left">
+            <a href="#" class="group flex flex-col items-center gap-2">
+                <span class="inline-flex h-20 w-20 items-center justify-center rounded-full bg-gray-200">
+                    <span class="text-xl font-medium leading-none text-white">ICON</span>
+                </span>
+                <p class="text-red-500 mt-2 ">
+                    Name</p>
+            </a>
+        </li>
+    </ul>
+</div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,11 +6,11 @@
                 <h1 class="text-2xl font-semibold text-gray-900">John's Dashboard</h1>
             </div>
             <div class="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-                <!-- Replace with your content -->
                 <div class="py-4">
-                    <div class="flex h-96 items-center justify-center rounded-lg border-4 border-gray-200 bg-gray-100"><p class="text-xl font-medium text-gray-500">Content comes in here</p></div>
+                    <div class="flex h-96 items-center justify-center rounded-lg">
+                        @include('components.cards.select-lunch')
+                    </div>
                 </div>
-                <!-- /End replace -->
             </div>
         </div>
     </main>
