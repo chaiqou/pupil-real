498cbdf90a36e288fe6716319218d2439c66d81b
chaiqou <lomtadzenikusha@gmail.com>
2022-10-31 19:29:04 +0400

    style: form inputs

--- resources/js/components/merchants/MerchantForm.vue
+++ resources/js/components/merchants/MerchantForm.vue
@@ -1,16 +1,30 @@
 <template>
     <Form @submit="onSubmitMerchantForm">
-        <Field placeholder="Title" v-model="title" name="title" type="text" />
+        <MerchantLabel label="Title" />
+        <Field
+            placeholder="Title"
+            v-model="title"
+            name="title"
+            type="text"
+            class="inline-block w-full placeholder-[#626262] rounded-md border border-[#626262] whitespace-nowrap"
+        />
+        <MerchantLabel label="Description" />
         <Field
             placeholder="Description"
             v-model="description"
             name="description"
             type="text"
+            class="inline-block w-full placeholder-[#626262] rounded-md border border-[#626262] whitespace-nowrap"
         />
         <MerchantLabel label="Active Range" />
         <Datepicker v-model="store.dateRange" range />
         <MerchantLabel label="Period Length" />
-        <Field v-model="period" name="period" type="number" />
+        <Field
+            v-model="period"
+            name="period"
+            type="number"
+            class="inline-block w-full placeholder-[#626262] rounded-md border border-[#626262] whitespace-nowrap"
+        />
         <MerchantRadioGroup />
         <MerchantLabel label="Holds" />
         <Datepicker v-model="store.holds" range />
