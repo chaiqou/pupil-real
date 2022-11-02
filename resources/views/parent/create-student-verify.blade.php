72352fbbc8a2cb42adc64ff56e027ae87804813f
pickle99 <jackrestler@gmail.com>
2022-11-02 14:19:31 +0400

    style: change card styles in create student verification, change buttons styles

--- resources/views/parent/create-student-verify.blade.php
+++ resources/views/parent/create-student-verify.blade.php
@@ -42,8 +42,8 @@
             <div class="mb-8 text-xl">
                 <p>Confirm student creation</p>
             </div>
-            <div class="w-[30rem]">
-                <img class="mx-auto w-auto" src="{{asset('img/pupilpay-with-lines.svg')}}" alt="PupilPay" />
+            <div class="md:w-[30rem]">
+                <img class="bg-gray-200 mx-auto rounded-md shadow-2xl" src="{{asset('img/pupilpay-with-lines.svg')}}" alt="PupilPay" />
                 <div>
                 <p class="mt-10 text-sm">
                     Are you sure you’re ready to create a new student and request a card?
@@ -54,7 +54,7 @@
                           <form method="POST" action="{{route('parent.create-student-verify_submit', ['student_id' => request()->student_id])}}">
                               @csrf
                               <input type="hidden" value="confirm" name="value"/>
-                              <button class="bg-green-400 p-3.5 rounded-md">Confirm</button>
+                              <button class="'inline-flex w-full justify-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm">Confirm</button>
                           </form>
                       </div>
 
@@ -62,7 +62,7 @@
                           <form method="POST" action="{{route('parent.create-student-verify_submit', ['student_id' => request()->student_id])}}">
                               @csrf
                               <input type="hidden" value="cancel" name="value"/>
-                              <button class="bg-red-400 p-3.5 rounded-md">Cancel</button>
+                              <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:w-auto sm:text-sm">Cancel</button>
                           </form>
                       </div>
                   </div>
