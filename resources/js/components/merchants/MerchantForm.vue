c5911a5eb184d8f0b5cf5e0a414fcc8b5f81d4e4
chaiqou <lomtadzenikusha@gmail.com>
2022-11-02 14:30:10 +0400

    refactor: remove stricted veevalidate validations and add yup validations

--- resources/js/components/merchants/MerchantForm.vue
+++ resources/js/components/merchants/MerchantForm.vue
@@ -1,11 +1,14 @@
 <template>
-    <Form class="mt-[4.2rem] w-96" @submit="onSubmitMerchantForm">
+    <Form
+        class="mt-[4.2rem] w-96"
+        :validation-schema="schema"
+        @submit="onSubmitMerchantForm"
+    >
         <MerchantLabel label="Title" />
         <Field
             v-model="store.title"
             name="title"
             type="text"
-            rules="required"
             class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
         />
         <MerchantErrorMessage name="title" />
@@ -14,7 +17,6 @@
             v-model="store.description"
             name="description"
             type="text"
-            rules="required"
             class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
         />
         <MerchantErrorMessage name="description" />
@@ -25,7 +27,6 @@
             v-model="store.period"
             name="period"
             type="number"
-            rules="required|numeric|min:1|max:31"
             class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
         />
         <MerchantErrorMessage name="period" />
@@ -50,21 +51,19 @@
         <MerchantLabel label="Price/Day" />
         <Field
             v-model="store.priceDay"
-            name="price day"
+            name="priceDay"
             type="number"
-            rules="required|numeric|min:1|max:99999"
             class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
         />
-        <MerchantErrorMessage name="price day" />
+        <MerchantErrorMessage name="priceDay" />
         <MerchantLabel label="Price/Period" />
         <Field
             v-model="store.pricePeriod"
-            name="price period"
+            name="pricePeriod"
             type="number"
-            rules="required|numeric|min:1|max:99999"
             class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
         />
-        <MerchantErrorMessage name="price period" />
+        <MerchantErrorMessage name="pricePeriod" />
         <button
             type="submit"
             class="inline-flex mt-4 w-full text-center items-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
@@ -81,11 +80,20 @@ import MerchantRadioGroup from "./MerchantRadioGroup.vue";
 import MerchantLabel from "./MerchantLabel.vue";
 import Multiselect from "@vueform/multiselect";
 import MerchantErrorMessage from "./MerchantErrorMessage.vue";
+import { object, string, mixed } from "yup";
 
 const store = useMerchantFormStore();
 
+const schema = object({
+    title: string().required(),
+    description: string().required(),
+    period: string().required().nullable(),
+    priceDay: string().required("Price/Day is required.").nullable(),
+    pricePeriod: string().required("Price/Period is required.").nullable(),
+});
+
 function onSubmitMerchantForm(value) {
-    console.log(value);
+    console.log(store.getMerchantData.dateRange);
 }
 
 function updateSelectedMeal(value) {
