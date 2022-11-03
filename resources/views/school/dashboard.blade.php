8d1df9a9f903ae9af11eb0753352ac0d3c20e5e1
chaiqou <lomtadzenikusha@gmail.com>
2022-11-03 15:34:19 +0400

    style: fix layout for school lunch managment

--- resources/views/school/dashboard.blade.php
+++ resources/views/school/dashboard.blade.php
@@ -1,13 +1,7 @@
 @extends('layouts.dashboard')
 @section('content')
-    <main >
-        <div class="py-12">
-            <div class="mx-2 max-w-7xl px-4 sm:px-6 md:px-8">
-                <div class="py-4 flex justify-between">
-                    <merchant-calendar :months={{ 11 }}></merchant-calendar>
-                    <merchant-form></merchant-form>
-                </div>
-            </div>
-        </div>
+    <main class="py-12 px-6 flex justify-between mx-2 ">
+        <merchant-calendar :months={{ 11 }}></merchant-calendar>
+        <merchant-form></merchant-form>
     </main>
 @endsection
