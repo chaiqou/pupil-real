ac7b02ed56776bf3711370a6c37008597175cfa0
chaiqou <lomtadzenikusha@gmail.com>
2022-11-03 16:11:11 +0400

    feat: move calendar and form from dashboard to lunch managment page with route

--- resources/views/school/dashboard.blade.php
+++ resources/views/school/dashboard.blade.php
@@ -1,7 +1,4 @@
 @extends('layouts.dashboard')
 @section('content')
-    <main class="py-12 px-6 flex justify-between mx-2 ">
-        <merchant-calendar :months={{ 11 }}></merchant-calendar>
-        <merchant-form></merchant-form>
-    </main>
+<div>Dashboard</div>
 @endsection
--- resources/views/school/lunch-management.blade.php
+++ resources/views/school/lunch-management.blade.php
@@ -1,4 +1,7 @@
 @extends('layouts.dashboard')
 @section('content')
-    <h1>Lunch Management</h1>
+<main class="py-12 px-6 flex justify-between mx-2 ">
+    <merchant-calendar :months={{ 11 }}></merchant-calendar>
+    <merchant-form></merchant-form>
+</main>
 @endsection
