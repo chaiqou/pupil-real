588405acb6d317ccc6984699e12ad563575179bf
chaiqou <lomtadzenikusha@gmail.com>
2022-11-04 13:59:00 +0400

    install: Axios for http requests * remove extra comments

--- app/Http/Controllers/api/LunchController.php
+++ app/Http/Controllers/api/LunchController.php
@@ -8,56 +8,31 @@ use Illuminate\Http\Request;
 
 class LunchController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Lunch  $lunch
-     * @return \Illuminate\Http\Response
-     */
+
     public function show(Lunch $lunch)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Lunch  $lunch
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(Request $request, Lunch $lunch)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Lunch  $lunch
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy(Lunch $lunch)
     {
         //
