49069f4e1fa2a331abf57ca499e487ed34620c10
chaiqou <lomtadzenikusha@gmail.com>
2022-11-04 13:57:06 +0400

    feat: add api resource routes for lunch * refactor lunch cntroller for Lunch model

--- app/Http/Controllers/api/LunchController.php
+++ app/Http/Controllers/api/LunchController.php
@@ -3,35 +3,62 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Lunch;
 use Illuminate\Http\Request;
 
 class LunchController extends Controller
 {
-
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         //
     }
 
-
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
         //
     }
 
-
-    public function show($id)
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Lunch  $lunch
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Lunch $lunch)
     {
         //
     }
 
-
-    public function update(Request $request, $id)
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Lunch  $lunch
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Lunch $lunch)
     {
         //
     }
 
-    public function destroy($id)
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Lunch  $lunch
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Lunch $lunch)
     {
         //
     }
