c1b9d75d0f8d2ac41ee2bc2d585595ac4b74b4ec
chaiqou <lomtadzenikusha@gmail.com>
2022-11-06 03:06:45 +0400

    refactor: Create reusable text input more vue 3 stylish

---
+++ resources/js/components/form-components/BaseInput.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <input
+            v-model="value"
+            type="text"
+            class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+        />
+        <span class="mt-2 text-sm text-red-500 whitespace-nowrap">{{
+            errorMessage
+        }}</span>
+    </div>
+</template>
+
+<script setup>
+import { useField } from "vee-validate";
+import { defineProps, toRef } from "vue";
+const props = defineProps({
+    name: {
+        type: String,
+        required: true,
+    },
+});
+function isRequired(value) {
+    if (value && value.trim()) {
+        return true;
+    }
+    return "This field is required";
+}
+
+const nameRef = toRef(props, "name");
+const { errorMessage, value } = useField(nameRef, isRequired);
+</script>
