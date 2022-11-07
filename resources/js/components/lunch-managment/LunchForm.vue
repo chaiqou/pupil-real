0aca19dfacd2d56196c99ea203e1f0e15a24dab3
chaiqou <lomtadzenikusha@gmail.com>
2022-11-07 12:17:40 +0400

    refactor: create seperate multiselect component for lunch

--- resources/js/components/lunch-managment/LunchForm.vue
+++ resources/js/components/lunch-managment/LunchForm.vue
@@ -4,6 +4,7 @@
         <BaseInput name="description" label="Description" />
         <RangeDatepicker name="active_range" label="Active range" />
         <BaseInput name="period_length" label="Period Length" />
+        <LunchMultiselect name="claimables" label="Claimables" />
         <RangeDatepicker name="holds" label="Holds" />
         <RangeDatepicker name="extras" label="Extras" />
         <BaseInput name="price_day" label="Price per day" />
@@ -15,6 +16,7 @@
 import axios from "../../config/axios/index";
 import BaseInput from "../form-components/BaseInput";
 import RangeDatepicker from "../form-components/RangeDatepicker.vue";
+import LunchMultiselect from "./LunchMultiselect.vue";
 
 function onSubmitMerchantForm(value) {
     axios
@@ -26,41 +28,4 @@ function onSubmitMerchantForm(value) {
             console.log(error);
         });
 }
-
-function updateSelectedMeal(value) {
-    console.log(value);
-}
-
-const mealOptions = [
-    "Breakfast",
-    "Lunch",
-    "Dinner",
-    "Snack",
-    "Dessert",
-    "Drink",
-    "Appetizer",
-    "Salad",
-    "Bread",
-    "Cereal",
-    "Soup",
-    "Beverage",
-    "Sauce",
-    "Marinade",
-    "Fingerfood",
-    "Salsa",
-    "Dip",
-];
 </script>
-
-<style src="@vueform/multiselect/themes/default.css" />
-<style>
-body {
-    --ms-bg: transparent;
-    --ms-tag-bg: #6c757d;
-    --ms-border-color: #6c757d;
-}
-
-.multiselect-tags-search {
-    background-color: inherit;
-}
-</style>
