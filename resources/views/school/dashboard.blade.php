6036198b12931559d4babf45b93f3cd96492600c
pickle99 <jackrestler@gmail.com>
2022-11-07 15:57:29 +0400

    style: add margin-bottom on school->dashboard

--- resources/views/school/dashboard.blade.php
+++ resources/views/school/dashboard.blade.php
@@ -18,7 +18,7 @@
         </div>
 
 
-        <div>
+        <div class="mb-32">
             <h1 class="text-xl my-5 p-5">Last transactions</h1>
             <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div class="inline-block w-fit py-2 align-middle md:px-6 lg:px-8">
