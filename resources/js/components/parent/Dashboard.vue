421d0e0be87c167dc3475d1997e081cd044af72f
pickle99 <jackrestler@gmail.com>
2022-11-07 16:14:10 +0400

    fix: send two factor code correctly on activate 2fa

--- resources/js/components/parent/Dashboard.vue
+++ resources/js/components/parent/Dashboard.vue
@@ -24,10 +24,10 @@
                </div>
            </dl>
 
-           <div>
+           <div class="lg:w-[45rem] w-full">
                <h1 class="text-xl my-5 p-5">Last transactions</h1>
                <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                   <div class="inline-block min-w-fit py-2 align-middle md:px-6 lg:px-8">
+                   <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
                        <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
                            <parent-dashboard-transactions :student-id="this.studentId"></parent-dashboard-transactions>
                        </div>
