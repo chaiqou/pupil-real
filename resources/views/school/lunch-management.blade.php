4f63c54a4b24e6256347c72d5dc10c37f6e9bdfd
chaiqou <lomtadzenikusha@gmail.com>
2022-11-07 21:04:24 +0400

    style: change styles for another months and make different current month * fix lunch-managment layout for tablets

--- resources/views/school/lunch-management.blade.php
+++ resources/views/school/lunch-management.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.dashboard')
 @section('content')
 <div class="flex md:py-12 md:px-12">
-    <calendar class="hidden md:flex" :months={{ 11 }}></calendar>
-    <lunch-form class="md:w-1/2 px-4 pb-6"></lunch-form>
+    <calendar class="hidden lg:flex" :months={{ 11 }}></calendar>
+    <lunch-form class="lg:w-1/2 px-4 pb-6"></lunch-form>
 </div>
 @endsection
