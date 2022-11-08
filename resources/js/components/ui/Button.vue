d2ff5e7b22d6c6ef15b2fe0ae296f59bfcc3c73b
chaiqou <lomtadzenikusha@gmail.com>
2022-11-08 22:52:16 +0400

    chore: fix button classes

--- resources/js/components/ui/Button.vue
+++ resources/js/components/ui/Button.vue
@@ -1,9 +1,9 @@
 <template>
     <button
         :type="type"
-        class="inline-flex mt-4 w-full text-center items-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        class="flex w-full justify-center mt-4 rounded-md px-4 py-2 bg-indigo-600 text-base font-medium text-white"
     >
-        {{ text }}
+        <p class="text-center">{{ text }}</p>
     </button>
 </template>
 
