b5d47925829af2b06d6636d2b9dec79310c86924
chaiqou <lomtadzenikusha@gmail.com>
2022-11-09 11:06:01 +0400

    refactor: make more reusable weekdays checkbox

--- resources/js/components/lunch-managment/WeekdaysCechkbox.vue
+++ resources/js/components/lunch-managment/WeekdaysCechkbox.vue
@@ -3,98 +3,21 @@
         >Weekdays
     </label>
     <div
+        v-for="day in dayOptions"
         class="flex items-center pl-4 rounded border border-gray-200 dark:border-gray-700"
     >
         <Field
             name="weekdays"
             type="checkbox"
-            value="Monday"
+            :value="day.fullName"
             rules="required"
-            id="Monday"
+            :id="day.fullName"
             class="w-4 text-indigo-600 bg-white rounded border-gray-300 focus:ring-indigo-500"
         />
         <label
-            for="Monday"
+            :for="day.fullName"
             class="py-4 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300"
-            >M</label
-        >
-        <Field
-            name="weekdays"
-            type="checkbox"
-            value="Tuesday"
-            rules="required"
-            id="Tuesday"
-            class="w-4 text-indigo-600 bg-white rounded border-gray-300 focus:ring-indigo-500"
-        />
-        <label
-            for="Tuesday"
-            class="py-4 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300"
-            >T</label
-        >
-        <Field
-            name="weekdays"
-            type="checkbox"
-            value="Wednesday"
-            rules="required"
-            id="Wednesday"
-            class="w-4 text-indigo-600 bg-white rounded border-gray-300 focus:ring-indigo-500"
-        />
-        <label
-            for="Wednesday"
-            class="py-4 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300"
-            >W</label
-        >
-        <Field
-            name="weekdays"
-            type="checkbox"
-            value="Thuersday"
-            rules="required"
-            id="Thuersday"
-            class="w-4 text-indigo-600 bg-white rounded border-gray-300 focus:ring-indigo-500"
-        />
-        <label
-            for="Thuersday"
-            class="py-4 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300"
-            >T</label
-        >
-        <Field
-            name="weekdays"
-            type="checkbox"
-            value="Friday"
-            rules="required"
-            id="Friday"
-            class="w-4 text-indigo-600 bg-white rounded border-gray-300 focus:ring-indigo-500"
-        />
-        <label
-            for="Friday"
-            class="py-4 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300"
-            >F</label
-        >
-        <Field
-            name="weekdays"
-            type="checkbox"
-            value="Saturday"
-            rules="required"
-            id="Saturday"
-            class="w-4 text-indigo-600 bg-white rounded border-gray-300 focus:ring-indigo-500"
-        />
-        <label
-            for="Saturday"
-            class="py-4 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300"
-            >S</label
-        >
-        <Field
-            name="weekdays"
-            type="checkbox"
-            value="Sunday"
-            rules="required"
-            id="Sunday"
-            class="w-4 text-blue-600 bg-white rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-        />
-        <label
-            for="Sunday"
-            class="py-4 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300"
-            >S</label
+            >{{ day.name }}</label
         >
     </div>
     <span class="mt-2 text-sm text-red-500 whitespace-nowrap">
@@ -104,4 +27,14 @@
 
 <script setup>
 import { Field, ErrorMessage } from "vee-validate";
+
+const dayOptions = [
+    { name: "M", fullName: "monday", validOption: true },
+    { name: "T", fullName: "tuesday", validOption: true },
+    { name: "W", fullName: "wednesday", validOption: true },
+    { name: "T", fullName: "thursday", validOption: true },
+    { name: "F", fullName: "friday", validOption: true },
+    { name: "S", fullName: "saturday", validOption: true },
+    { name: "S", fullName: "sunday", validOption: true },
+];
 </script>
