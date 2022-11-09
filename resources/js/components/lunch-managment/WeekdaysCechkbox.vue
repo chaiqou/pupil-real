28877ee057729744b026e85b38871404b8ce76e4
chaiqou <lomtadzenikusha@gmail.com>
2022-11-09 12:20:52 +0400

    refactor(style): make checkboxes style like radio buttons

--- resources/js/components/lunch-managment/WeekdaysCechkbox.vue
+++ resources/js/components/lunch-managment/WeekdaysCechkbox.vue
@@ -1,24 +1,32 @@
 <template>
-    <label class="text-md flex font-bold text-gray-600 whitespace-normal"
-        >Weekdays
-    </label>
-    <div
-        v-for="day in dayOptions"
-        class="flex items-center pl-4 rounded border border-gray-200 dark:border-gray-700"
-    >
-        <Field
-            name="weekdays"
-            type="checkbox"
-            :value="day.fullName"
-            rules="required"
-            :id="day.fullName"
-            class="w-4 text-indigo-600 bg-white rounded border-gray-300 focus:ring-indigo-500"
-        />
-        <label
-            :for="day.fullName"
-            class="py-4 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300"
-            >{{ day.name }}</label
-        >
+    <div class="flex items-center justify-between">
+        <label class="text-md flex font-bold text-gray-600 whitespace-normal"
+            >Weekdays
+        </label>
+    </div>
+    <div class="flex justify-center">
+        <ul v-for="day in dayOptions" class="grid w-full md:grid-cols-3">
+            <li>
+                <Field
+                    name="weekdays"
+                    type="checkbox"
+                    :value="day.fullName"
+                    rules="required"
+                    :id="day.fullName"
+                    class="hidden peer"
+                />
+                <label
+                    :for="day.fullName"
+                    class="flex justify-between items-center p-5 w-full text-gray-500 bg-white rounded-lg border-2 border-gray-200 cursor-pointer peer-checked:bg-indigo-600 peer-checked:border-indigo-600 hover:text-gray-600 peer-checked:text-white hover:bg-gray-50"
+                >
+                    <div class="block">
+                        <div class="w-full text-lg font-semibold">
+                            {{ day.name }}
+                        </div>
+                    </div>
+                </label>
+            </li>
+        </ul>
     </div>
     <span class="mt-2 text-sm text-red-500 whitespace-nowrap">
         <ErrorMessage name="weekdays" />
