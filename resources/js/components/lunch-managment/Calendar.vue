0cf469a05d176c6b55e397aeeff7d61ee155d337
chaiqou <lomtadzenikusha@gmail.com>
2022-11-11 11:47:09 +0400

    style: finish calendar styling

--- resources/js/components/lunch-managment/Calendar.vue
+++ resources/js/components/lunch-managment/Calendar.vue
@@ -1,64 +1,59 @@
 <template>
-    <div class="invisible lg:visible w-full">
-        <div class="bg-inherit">
-            <div
-                class="mx-auto 2xl:w-[100vh] grid grid-cols-1 gap-x-8 gap-y-16 px-4 py-16 sm:grid-cols-2 sm:px-6 xl:max-w-none 2xl:grid-cols-2 xl:px-8"
+    <div class="invisible lg:visible bg-inherit">
+        <div
+            class="mx-auto grid max-w-3xl 2xl:w-[100vh] grid-cols-1 gap-x-8 gap-y-16 px-4 py-16 sm:grid-cols-2 sm:px-6 xl:max-w-none xl:grid-cols-3 xl:px-8 2xl:grid-cols-3"
+        >
+            <section
+                v-for="month in monthsDays"
+                :key="month.name"
+                class="text-center"
             >
-                <section
-                    v-for="month in monthsDays"
-                    :key="month.name"
-                    class="text-center"
+                <h2 class="font-semibold text-gray-900">
+                    {{ month.name }} {{ month.year }}
+                </h2>
+                <div
+                    class="mt-6 grid grid-cols-7 text-xs leading-6 text-gray-500"
                 >
-                    <h2 class="font-semibold text-gray-900">
-                        {{ month.name }} {{ month.year }}
-                    </h2>
-                    <div
-                        class="mt-6 grid grid-cols-7 text-xs leading-6 text-gray-500"
+                    <div>M</div>
+                    <div>T</div>
+                    <div>W</div>
+                    <div>T</div>
+                    <div>F</div>
+                    <div>S</div>
+                    <div>S</div>
+                </div>
+                <div
+                    class="isolate mt-2 grid grid-cols-7 gap-px rounded-lg bg-gray-200 text-sm shadow ring-1 ring-gray-200"
+                >
+                    <button
+                        v-for="(day, dayIdx) in month.days"
+                        :key="day.date"
+                        type="button"
+                        :class="[
+                            month.name !== getMonthByIndex(day.getMonth()) &&
+                            month.name === monthFullNames[day.getMonth()]
+                                ? 'bg-white text-gray-900'
+                                : 'bg-gray-50 text-gray-400',
+                            dayIdx === 0 && 'rounded-tl-lg',
+                            dayIdx === 6 && 'rounded-tr-lg',
+                            dayIdx === month.days.length - 7 && 'rounded-bl-lg',
+                            dayIdx === month.days.length - 1 && 'rounded-br-lg',
+                            'py-1.5 hover:bg-gray-100 focus:z-10',
+                        ]"
                     >
-                        <div>M</div>
-                        <div>T</div>
-                        <div>W</div>
-                        <div>T</div>
-                        <div>F</div>
-                        <div>S</div>
-                        <div>S</div>
-                    </div>
-                    <div
-                        class="isolate mt-2 grid grid-cols-7 gap-px rounded-lg bg-gray-200 text-sm shadow ring-1 ring-gray-200"
-                    >
-                        <button
-                            v-for="(day, dayIdx) in month.days"
-                            :key="day.date"
-                            type="button"
+                        <time
+                            :datetime="format(day, 'yyyy-MM-dd')"
                             :class="[
-                                month.name !==
-                                    getMonthByIndex(day.getMonth()) &&
-                                month.name === monthFullNames[day.getMonth()]
-                                    ? 'bg-white text-gray-900'
-                                    : 'bg-gray-50 text-gray-400',
-                                dayIdx === 0 && 'rounded-tl-lg',
-                                dayIdx === 6 && 'rounded-tr-lg',
-                                dayIdx === month.days.length - 7 &&
-                                    'rounded-bl-lg',
-                                dayIdx === month.days.length - 1 &&
-                                    'rounded-br-lg',
-                                'py-1.5 hover:bg-gray-100 focus:z-10',
+                                isToday(day) &&
+                                    'bg-indigo-600 font-semibold text-white',
+                                'mx-auto flex h-7 w-7 items-center justify-center rounded-full',
                             ]"
                         >
-                            <time
-                                :datetime="format(day, 'yyyy-MM-dd')"
-                                :class="[
-                                    isToday(day) &&
-                                        'bg-indigo-600 font-semibold text-white',
-                                    'mx-auto flex h-7 w-7 items-center justify-center rounded-full',
-                                ]"
-                            >
-                                {{ format(day, "d") }}
-                            </time>
-                        </button>
-                    </div>
-                </section>
-            </div>
+                            {{ format(day, "d") }}
+                        </time>
+                    </button>
+                </div>
+            </section>
         </div>
     </div>
 </template>
