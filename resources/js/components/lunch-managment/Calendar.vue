2c4548ca1569c093ad521c07b55267f02148eb12
chaiqou <lomtadzenikusha@gmail.com>
2022-11-12 22:30:57 +0400

    wip: style form

--- resources/js/components/lunch-managment/Calendar.vue
+++ resources/js/components/lunch-managment/Calendar.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="bg-inherit">
+        <div class="bg-inherit w-[50vw] h-[100vh]">
             <div
                 class="mx-auto grid max-w-3xl grid-cols-1 gap-x-8 gap-y-16 px-4 py-16 sm:grid-cols-2 sm:px-6 xl:max-w-none xl:grid-cols-3 xl:px-8 2xl:grid-cols-3"
             >
--- resources/views/school/lunch-management.blade.php
+++ resources/views/school/lunch-management.blade.php
@@ -5,6 +5,7 @@
       <div class="mx-auto max-w-7xl px-4 sm:px-6 md:px-6">
       <div class="py-4 flex">
         <calendar class="hidden lg:flex" :months={{ 11 }}></calendar>
+        <lunch-form></lunch-form>
         </div>
       </div>
     </div>
