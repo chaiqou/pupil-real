1b9103be24ffe478d676d28850f5f9897f199874
chaiqou <lomtadzenikusha@gmail.com>
2022-11-15 13:56:05 +0400

    feat: create Lunch form Pinai state

---
+++ resources/js/stores/useLunchFormStore.js
@@ -0,0 +1,34 @@
+import { defineStore } from "pinia";
+
+export const useLunchFormStore = defineStore("lunch", {
+    state: () => {
+        return {
+            title: "",
+            description: "",
+            period_length: "",
+            tags: [],
+            active_range: [],
+            claimables: [],
+            holds: [],
+            extras: [],
+            price_day: "",
+            price_period: "",
+        };
+    },
+    getters: {
+        getLunchFormData() {
+            return {
+                title: this.title,
+                description: this.description,
+                period_length: this.period_length,
+                tags: this.tags,
+                active_range: this.active_range,
+                claimables: this.claimables,
+                holds: this.holds,
+                extras: this.extras,
+                price_day: this.price_day,
+                price_period: this.price_period,
+            };
+        },
+    },
+});
