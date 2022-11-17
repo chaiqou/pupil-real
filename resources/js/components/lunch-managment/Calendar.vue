d1b323bc1e33eaba98963d28e809244a5a4bb753
chaiqou <lomtadzenikusha@gmail.com>
2022-11-17 13:53:20 +0400

    style: fix layout for ipads

--- resources/js/components/lunch-managment/Calendar.vue
+++ resources/js/components/lunch-managment/Calendar.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-        <div class="bg-inherit w-[50vw] h-[100vh]">
+        <div
+            class="bg-inherit md:w-[30vw] md:h-[70vh] xl:w-[50vw] xl:h-[100vh]"
+        >
             <div
                 class="mx-auto grid max-w-3xl grid-cols-1 gap-x-8 gap-y-16 px-4 py-16 sm:grid-cols-1 sm:px-6 xl:max-w-none xl:grid-cols-2 xl:px-8 2xl:grid-cols-3"
             >
