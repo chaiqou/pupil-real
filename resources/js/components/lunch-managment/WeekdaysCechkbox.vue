fe636ed682a5badf15349e6bc77a340276154b22
chaiqou <lomtadzenikusha@gmail.com>
2022-11-17 18:12:57 +0400

    chore: 7 col in one line

--- resources/js/components/lunch-managment/WeekdaysCechkbox.vue
+++ resources/js/components/lunch-managment/WeekdaysCechkbox.vue
@@ -1,6 +1,6 @@
 <template>
     <label class="text-md font-bold text-gray-600">Weekdays</label>
-    <div class="grid grid-cols-3 gap-3 sm:grid-cols-6 text-center">
+    <div class="grid grid-cols-3 gap-3 sm:grid-cols-7 text-center">
         <ul v-for="day in dayOptions">
             <li>
                 <Field
