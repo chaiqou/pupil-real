5379ce44289fb78bc55dae8128311f95352c9cd3
chaiqou <lomtadzenikusha@gmail.com>
2022-12-05 14:28:49 +0400

    feat: add and registere lucn list component

--- resources/views/school/lunch-management.blade.php
+++ resources/views/school/lunch-management.blade.php
@@ -1,4 +1,7 @@
 @extends('layouts.dashboard')
 @section('content')
-
+<lunch-list></lunch-list>
+<a href="{{route('school.add-lunch')}}" class="hidden md:flex bg-green-500 hover:bg-green-600 w-fit rounded-md text-white px-4 py-2 items-center">
+    <p class="ml-1.5">Add Lunch</p>
+</a>
 @endsection
