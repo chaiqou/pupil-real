03c622d6033a2c5871e4ea0366a4e69317a83836
chaiqou <lomtadzenikusha@gmail.com>
2022-12-13 14:12:44 +0400

    chore: remove unused routes from Ziggy configuration file

--- resources/js/ziggy.js
+++ resources/js/ziggy.js
@@ -3,38 +3,6 @@ const Ziggy = {
     port: null,
     defaults: {},
     routes: {
-        "debugbar.openhandler": {
-            uri: "_debugbar/open",
-            methods: ["GET", "HEAD"],
-        },
-        "debugbar.clockwork": {
-            uri: "_debugbar/clockwork/{id}",
-            methods: ["GET", "HEAD"],
-        },
-        "debugbar.assets.css": {
-            uri: "_debugbar/assets/stylesheets",
-            methods: ["GET", "HEAD"],
-        },
-        "debugbar.assets.js": {
-            uri: "_debugbar/assets/javascript",
-            methods: ["GET", "HEAD"],
-        },
-        "debugbar.cache.delete": {
-            uri: "_debugbar/cache/{key}/{tags?}",
-            methods: ["DELETE"],
-        },
-        "ignition.healthCheck": {
-            uri: "_ignition/health-check",
-            methods: ["GET", "HEAD"],
-        },
-        "ignition.executeSolution": {
-            uri: "_ignition/execute-solution",
-            methods: ["POST"],
-        },
-        "ignition.updateConfig": {
-            uri: "_ignition/update-config",
-            methods: ["POST"],
-        },
         "parent.update-student_api": {
             uri: "api/parent/update-student",
             methods: ["POST"],
