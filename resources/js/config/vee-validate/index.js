2c432c1e68b47e0fa278f88d0ef34567acfb0de5
chaiqou <lomtadzenikusha@gmail.com>
2022-12-14 21:46:24 +0400

    uninstall: vee-validate/rules package

--- resources/js/config/vee-validate/index.js
+++ resources/js/config/vee-validate/index.js
@@ -1,6 +1,4 @@
-import { defineRule } from "vee-validate";
-import { required, min, max, numeric } from "@vee-validate/rules";
-import { configure } from "vee-validate";
+import { defineRule, configure } from "vee-validate";
 
 configure({
     validateOnBlur: true,
@@ -8,15 +6,10 @@ configure({
     validateOnInput: true,
 });
 
-defineRule("required", required);
-defineRule("min", min);
-defineRule("max", max);
-defineRule("numeric", numeric);
-
 defineRule("email", (value) => {
     const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (!regexEmail.test(value)) {
-        return ;
+        return;
     }
     return true;
 });
@@ -24,15 +17,16 @@ defineRule("email", (value) => {
 defineRule("min-words-3", (value) => {
     const regexMinWords = /^([a-z0-9]*[a-z]){3}[a-z0-9]*$/i;
     if (!regexMinWords.test(value)) {
-        return ;
+        return;
     }
     return true;
 });
 
 defineRule("phone", (value) => {
-    const regexPhoneNumber = /^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$/;
+    const regexPhoneNumber =
+        /^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$/;
     if (!regexPhoneNumber.test(value)) {
-        return ;
+        return;
     }
     return true;
 });
