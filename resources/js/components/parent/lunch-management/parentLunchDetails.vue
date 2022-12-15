7929da5ac32e16b0ae77a5f10d2862d08af48fb2
chaiqou <lomtadzenikusha@gmail.com>
2022-12-15 15:09:10 +0400

    style: create parent luch details card

---
+++ resources/js/components/parent/lunch-management/parentLunchDetails.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h3 class="text-lg font-medium leading-6 text-gray-900">
+            Applicant Information
+        </h3>
+        <p class="mt-1 max-w-2xl text-sm text-gray-500">
+            Personal details and application.
+        </p>
+    </div>
+    <div class="mt-5 border-t border-gray-200">
+        <dl class="divide-y divide-gray-200">
+            <div class="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5">
+                <dt class="text-sm font-medium text-gray-500">Dasaxeleba</dt>
+                <dd
+                    class="mt-1 flex text-sm text-gray-900 sm:col-span-2 sm:mt-0"
+                >
+                    <span class="flex-grow">Value</span>
+                </dd>
+            </div>
+        </dl>
+    </div>
+</template>
+
+<script setup></script>
