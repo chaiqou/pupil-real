ab3ca2b60deee9b9bb78e2bed2310a11ddbf07c4
chaiqou <lomtadzenikusha@gmail.com>
2022-12-21 16:49:36 +0400

    wip: find apporpiate student and save information what we need form student modal

--- app/Http/Controllers/Parent/Api/OrderLunchController.php
+++ app/Http/Controllers/Parent/Api/OrderLunchController.php
@@ -3,13 +3,23 @@
 namespace App\Http\Controllers\Parent\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\PeriodicLunch;
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class OrderLunchController extends Controller
 {
     public function index(Request $request)
     {
-        dump($request->all());
+
+        $student = Student::where('id', $request->student_id)->first();
+
+        $lunch = PeriodicLunch::create([
+            'student_id' => $student->id,
+            'card_data' => $student->card_data,
+            'transaction_id' => 123,
+            'merchant_id' => $student->school_id,
+        ]);
 
         return response()->json(['success' => 'success']);
     }
