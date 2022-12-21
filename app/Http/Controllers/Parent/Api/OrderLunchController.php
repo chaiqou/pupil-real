8033e32f9b71ae88580acc5a86c654b4a2daea71
chaiqou <lomtadzenikusha@gmail.com>
2022-12-21 18:38:44 +0400

    feat: calculate available dates

--- app/Http/Controllers/Parent/Api/OrderLunchController.php
+++ app/Http/Controllers/Parent/Api/OrderLunchController.php
@@ -6,13 +6,31 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Parent\LunchOrderRequest;
 use App\Models\PeriodicLunch;
 use App\Models\Student;
+use Carbon\Carbon;
 
 class OrderLunchController extends Controller
 {
     public function index(LunchOrderRequest $request)
     {
         $validate = $request->validated();
-        $student = Student::where('id', $validate->student_id)->first();
+        dump($validate);
+        $student = Student::where('id', $validate['student_id'])->first();
+
+        // Validate the start date
+        $startDate = Carbon::parse($validate['start_day']);
+        dump($startDate->toDateString());
+
+        // Filter the available dates array and take the first n elements
+        $availableDates = array_filter($validate['available_days'], function ($date) use ($startDate) {
+            return Carbon::parse($date)->gte($startDate);
+        });
+
+        $availableDates = array_slice($availableDates, 0, $validate['period_length']);
+
+        dump($availableDates);
+
+
+
 
         $lunch = PeriodicLunch::create([
             'student_id' => $student->id,
