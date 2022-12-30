e219c2ff3c8f779ab87a34f28a03672e71b9f751
pickle99 <jackrestler@gmail.com>
2022-12-30 15:38:19 +0400

    refactor: remove function duplicate in RequestProformasByQuery command, make post only if transaction is found

--- app/Console/Commands/RequestProformasByQuery.php
+++ app/Console/Commands/RequestProformasByQuery.php
@@ -37,79 +37,46 @@ class RequestProformasByQuery extends Command
             $billingoData = BillingoData::where('merchant_id', $merchant->id)->first();
             $request = Http::withHeaders([
                 'X-API-KEY' => $billingoData->billingo_api_key,
-            ])->get("https://api.billingo.hu/v3/documents?type=proforma&per_page=100&payment_status=paid&paid_start_date=$yesterday_date")->json();
-            $firstRequest = [];
-            array_push($firstRequest, $request['data']);
-
-            $foreachChecker = $request;
+            ])->get("https://api.billingo.hu/v3/documents?type=proforma&per_page=1&payment_status=paid&paid_start_date=$yesterday_date")->json();
+            $dataArray = array();
+            array_push($dataArray, $request['data']);
             if($request['next_page_url'] !== null)
             {
-                $dataArray = array();
-                array_push($dataArray, $request['data']);
                 do {
                     $requestLoop =  Http::withHeaders([
                         'X-API-KEY' => $billingoData->billingo_api_key,
                     ])->get($request['next_page_url'])->json();
-
                     $request = $requestLoop;
-
                     array_push($dataArray, $requestLoop['data']);
                 }while ($request['next_page_url'] !== null);
             }
         }
-        if($foreachChecker["next_page_url"] !== null)
-        {
             foreach($dataArray as $datas) {
                 foreach($datas as $data) {
                     $transaction = Transaction::where('billingo_proforma_id', $data["id"])->first();
-                    $merchantOfTransaction = Merchant::where('id', $transaction->merchant_id)->first();
-                    $merchantBillingoData = BillingoData::where('merchant_id', $merchantOfTransaction->id)->first();
-                    $request = Http::withHeaders([
-                        'X-API-KEY' => $merchantBillingoData->billingo_api_key,
-                    ])->post("https://api.billingo.hu/v3/documents/" . $data["id"] . "/create-from-proforma", [
-                        "document_type" => "invoice",
-                        "fulfillment_date" => $data["fulfillment_date"],
-                        "due_date" => $data["due_date"],
-                        "document_format" => "",
-                        "comment" => $transaction->billing_comment,
-                        "settings" => [
-                            "should_send_email" => true,
-                        ]
-                    ])->json();
-                    $transaction->update([
-                        'billing_type' => 'invoice',
-                        'billingo_transaction_id' => $request['id']
-                    ]);
-                    return;
-                }
-            }
-        }
-
-        if($foreachChecker["next_page_url"] === null) {
-            foreach($firstRequest as $datas) {
-                foreach($datas as $data) {
-                    $transaction = Transaction::where('billingo_proforma_id', $data["id"])->first();
-                    $merchantOfTransaction = Merchant::where('id', $transaction->merchant_id)->first();
-                    $merchantBillingoData = BillingoData::where('merchant_id', $merchantOfTransaction->id)->first();
-                    $request = Http::withHeaders([
-                        'X-API-KEY' => $merchantBillingoData->billingo_api_key,
-                    ])->post("https://api.billingo.hu/v3/documents/" . $data["id"] . "/create-from-proforma", [
-                        "document_type" => "invoice",
-                        "fulfillment_date" => $data["fulfillment_date"],
-                        "due_date" => $data["due_date"],
-                        "document_format" => "",
-                        "comment" => $transaction->billing_comment,
-                        "settings" => [
-                            "should_send_email" => true,
+                    if($transaction !== null)
+                    {
+                        $merchantOfTransaction = Merchant::where('id', $transaction->merchant_id)->first();
+                        $merchantBillingoData = BillingoData::where('merchant_id', $merchantOfTransaction->id)->first();
+                        $request = Http::withHeaders([
+                            'X-API-KEY' => $merchantBillingoData->billingo_api_key,
+                        ])->post("https://api.billingo.hu/v3/documents/" . $data["id"] . "/create-from-proforma", [
+                            "document_type" => "invoice",
+                            "fulfillment_date" => $data["fulfillment_date"],
+                            "due_date" => $data["due_date"],
+                            "document_format" => "",
+                            "comment" => $transaction->billing_comment,
+                            "settings" => [
+                                "should_send_email" => true,
                             ]
-                    ])->json();
-                    $transaction->update([
-                        'billing_type' => 'invoice',
-                        'billingo_transaction_id' => $request['id']
-                    ]);
-                   return;
+                        ])->json();
+                        $transaction->update([
+                            'billing_type' => 'invoice',
+                            'billingo_transaction_id' => $request['id']
+                        ]);
+                        return;
+                    }
                 }
             }
-        }
     }
 }
