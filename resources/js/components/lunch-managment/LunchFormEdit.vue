ed17166902c8b93b843373ff959f21b677e56c1e
pickle99 <jackrestler@gmail.com>
2023-01-05 19:07:34 +0400

    feat: round fee price

--- resources/js/components/lunch-managment/LunchFormEdit.vue
+++ resources/js/components/lunch-managment/LunchFormEdit.vue
@@ -280,7 +280,7 @@ const dataIsLoaded = ref(false);
 
 const afterFeeCanBeCalculated = ref(false);
 const afterFeesCalculate = () => {
-    store.after_fees = (Number(store.price_period) + 85) / (1 - (7/500));
+    store.after_fees = Math.round((Number(store.price_period) + 85) / (1 - (7/500)));
     store.price_period = store.after_fees;
     afterFeeCanBeCalculated.value = false;
     setFieldValue('Price Period', store.price_period);
