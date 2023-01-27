da5337d3b9f62850b2360a253d0dad3fed210b26
chaiqou <lomtadzenikusha@gmail.com>
2023-01-27 14:36:47 +0400

    refactor: update transactions table

--- database/migrations/2022_10_20_150000_create_transactions_table.php
+++ database/migrations/2022_10_20_150000_create_transactions_table.php
@@ -20,16 +20,17 @@ return new class() extends Migration
             $table->foreignId('merchant_id')->constrained();
             $table->string('transaction_identifier');
             $table->date('transaction_date');
-            $table->string('transaction_amount');
-            $table->string('transaction_type');
-            $table->string('comments');
-            $table->string('history');
-            $table->string('wallet');
-            $table->string('stripe_session_id');
-            $table->string('payment_method');
-            $table->string('billing_items');
-            $table->string('billing_provider');
-            $table->string('billing_comment');
+            $table->bigInteger('transaction_amount');
+            $table->enum('transaction_type',["payment", "storno", "top_up"]);
+            $table->json('comments')->default(json_encode(['comments' => ['comment' => null, 'comment_by' => null, 'comment_at' => null]]));
+            $table->json('history')->default(json_encode(['history' => ['data' => null, 'data_by' => null, 'data_at' => null]]));
+            $table->string('wallet_id')->nullable();
+            /* Update wallet to be constrained to wallet table */
+            $table->string('stripe_session_id')->nullable();
+            $table->enum('payment_method',["stripe", "bank_transfer", "wallet", "other", "none"]);
+            $table->json('billing_items')->nullable();
+            $table->enum('billing_provider',["none","billingo"]);
+            $table->json('billing_comment')->nullable();
             $table->integer('invoice_id');
             $table->timestamps();
         });
