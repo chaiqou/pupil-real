bf022ac1a992c4aab50174b25313ccc39cb81945
chaiqou <lomtadzenikusha@gmail.com>
2023-01-27 13:57:35 +0400

    wip: update transaction and pending transaction tables

--- database/migrations/2023_01_27_102657_create_pending_transactions_table.php
+++ database/migrations/2023_01_27_102657_create_pending_transactions_table.php
@@ -15,6 +15,26 @@ return new class extends Migration
     {
         Schema::create('pending_transactions', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('student_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('merchant_id')->constrained();
+            $table->string('transaction_identifier');
+            $table->date('transaction_date');
+            $table->string('transaction_amount');
+            $table->string('transaction_type');
+            $table->string('comments');
+            $table->string('history');
+            $table->string('wallet');
+            $table->string('stripe_session_id');
+            $table->string('payment_method');
+            $table->string('billing_items');
+            $table->string('billing_provider');
+            $table->string('billing_comment');
+            $table->string('billing_type');
+            $table->integet('proforma_id')->nullable();
+            $table->string('convert_to_invoice');
+            $table->string('handler_status');
+            $table->string('cancelled_status');
             $table->timestamps();
         });
     }
