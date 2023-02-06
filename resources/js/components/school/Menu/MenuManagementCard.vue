8ff08a32bcefccd44a6bfa4e85ac4fdb4d52450b
chaiqou <lomtadzenikusha@gmail.com>
2023-02-06 19:00:56 +0400

    style:  menu management popup markup and styles

--- resources/js/components/school/Menu/MenuManagementCard.vue
+++ resources/js/components/school/Menu/MenuManagementCard.vue
@@ -5,98 +5,45 @@
         <div
             class="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10 w-full"
         >
-            <h1 class="text-gray-700 mb-2 font-semibold">
-                {{ format(store.selected_day, "yyyy MMMM dd") }}
-            </h1>
+            <template
+                class="flex flex-wrap justify-between border-b border-gray-200"
+            >
+                <h1 class="text-gray-700 mb-2 font-semibold">
+                    {{ format(store.selected_day, "yyyy MMMM dd") }}
+                </h1>
+                <h2 class="text-gray-700 mb-2 font-semibold">Lunch Name</h2>
+            </template>
+
             <div class="mx-auto max-w-md w-full">
                 <div class="flex items-start space-x-4">
-                    <div class="min-w-0 flex-1">
-                        <Form ref="target">
-                            <div
-                                class="border-b border-gray-200 focus-within:border-indigo-600"
+                    <Form class="min-w-0 flex-1" ref="target">
+                        <div
+                            class="flex w-full justify-between space-x-12 my-4"
+                        >
+                            <button
+                                type="button"
+                                class="items-center p-4 w-full rounded-md border border-gray-300 bg-white text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                             >
-                                <Field
-                                    name="Comment"
-                                    as="textarea"
-                                    class="block w-full resize-none border-0 border-b border-transparent p-0 pb-2 focus:border-indigo-600 focus:ring-0 sm:text-sm"
-                                    placeholder="Menu description..."
-                                ></Field>
-                            </div>
-                            <div class="flex justify-between pt-2">
-                                <div class="flex-shrink-0">
-                                    <div>
-                                        <div class="relative mt-1">
-                                            <button
-                                                @click="toggleSelect"
-                                                type="button"
-                                                class="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
-                                            >
-                                                <span class="block truncate"
-                                                    >2 Lunches Selected</span
-                                                >
-                                                <span
-                                                    class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2"
-                                                >
-                                                    <LunchesArrowIcon />
-                                                </span>
-                                            </button>
-                                            <ul
-                                                v-if="store.toggle_select"
-                                                class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
-                                            >
-                                                <li
-                                                    class="relative cursor-default select-none py-2 pl-8 pr-4 text-gray-900"
-                                                    id="listbox-option-0"
-                                                    role="option"
-                                                >
-                                                    <span
-                                                        class="block truncate font-semibold"
-                                                        >Test lunch</span
-                                                    >
-                                                    <span
-                                                        class="absolute inset-y-0 left-0 flex items-center pl-1.5 text-indigo-600"
-                                                    >
-                                                        <CheckIcon />
-                                                    </span>
-                                                </li>
-                                                <li
-                                                    class="relative cursor-default select-none py-2 pl-8 pr-4 text-gray-900"
-                                                    id="listbox-option-0"
-                                                    role="option"
-                                                >
-                                                    <span
-                                                        class="block truncate font-semibold"
-                                                        >Lunch title</span
-                                                    >
-                                                    <span
-                                                        class="absolute inset-y-0 left-0 flex items-center pl-1.5 text-indigo-600"
-                                                    >
-                                                        <CheckIcon />
-                                                    </span>
-                                                </li>
-                                                <li
-                                                    class="relative cursor-default select-none py-2 pl-8 pr-4 text-gray-900"
-                                                    id="listbox-option-0"
-                                                    role="option"
-                                                >
-                                                    <span
-                                                        class="block truncate font-normal"
-                                                        >Not selected</span
-                                                    >
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                                <button
-                                    type="button"
-                                    class="inline-flex items-center rounded-md border border-gray-300 bg-white px-3 mt-1 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                                >
-                                    Add another
-                                </button>
-                            </div>
-                        </Form>
-                    </div>
+                                Fixed
+                            </button>
+                            <button
+                                type="button"
+                                class="items-center p-4 w-full rounded-md border border-gray-300 bg-white text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            >
+                                Choices
+                            </button>
+                        </div>
+                        <div
+                            class="border-y p-2 border-gray-200 focus-within:border-indigo-600"
+                        >
+                            <Field
+                                name="Comment"
+                                as="textarea"
+                                class="block w-full resize-none border-transparent p-0 pb-2 focus:border-indigo-600 focus:rounded-lg focus:border-none focus:ring-0 sm:text-sm"
+                                placeholder="Menu description..."
+                            ></Field>
+                        </div>
+                    </Form>
                 </div>
             </div>
         </div>
@@ -104,8 +51,6 @@
 </template>
 
 <script setup>
-import LunchesArrowIcon from "@/components/icons/LunchesArrowIcon.vue";
-import CheckIcon from "@/components/icons/checkIcon.vue";
 import { ref } from "vue";
 import { onClickOutside } from "@vueuse/core";
 import { useTooltipStore } from "@/stores/useTooltipStore";
@@ -124,7 +69,7 @@ const store = useTooltipStore();
 
 // Close clickoutside
 const target = ref(null);
-onClickOutside(target, (event) => (store.toggle_tooltip = false));
+onClickOutside(target, () => (store.toggle_tooltip = false));
 
 // close select component
 
