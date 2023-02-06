37b44154d199cde083d122cd82443aa1d40d080c
chaiqou <lomtadzenikusha@gmail.com>
2023-02-06 19:45:44 +0400

    style: remove unused divs and make responsive design for card

--- resources/js/components/school/Menu/MenuManagementCard.vue
+++ resources/js/components/school/Menu/MenuManagementCard.vue
@@ -1,9 +1,9 @@
 <template>
     <div
-        class="fixed flex min-h-screen min-w-[100vw] md:min-w-[90vw] flex-col z-50 justify-center overflow-hidden bg-black/50 py-6 sm:py-12"
+        class="fixed flex min-h-screen w-screen flex-col z-50 justify-center items-center bg-black/50"
     >
         <div
-            class="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10 w-full"
+            class="bg-white px-6 py-10 shadow-2xl sm:max-w-lg rounded-lg w-full"
         >
             <template
                 class="flex flex-wrap justify-between border-b border-gray-200"
@@ -14,38 +14,34 @@
                 <h2 class="text-gray-700 mb-2 font-semibold">Lunch Name</h2>
             </template>
 
-            <div class="mx-auto max-w-md w-full">
-                <div class="flex items-start space-x-4">
-                    <Form class="min-w-0 flex-1" ref="target">
-                        <div
-                            class="flex w-full justify-between space-x-12 my-4"
-                        >
-                            <button
-                                type="button"
-                                class="items-center p-4 w-full rounded-md border border-gray-300 bg-white text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                            >
-                                Fixed
-                            </button>
-                            <button
-                                type="button"
-                                class="items-center p-4 w-full rounded-md border border-gray-300 bg-white text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                            >
-                                Choices
-                            </button>
-                        </div>
-                        <div
-                            class="border-y p-2 border-gray-200 focus-within:border-indigo-600"
-                        >
-                            <Field
-                                name="Comment"
-                                as="textarea"
-                                class="block w-full resize-none border-transparent p-0 pb-2 focus:border-indigo-600 focus:rounded-lg focus:border-none focus:ring-0 sm:text-sm"
-                                placeholder="Menu description..."
-                            ></Field>
-                        </div>
-                    </Form>
-                </div>
-            </div>
+            <Form ref="target">
+                <template
+                    class="flex flex-wrap space-y-4 w-full justify-between sm:space-y-0 sm:flex-nowrap sm:space-x-12 my-4"
+                >
+                    <button
+                        type="button"
+                        class="items-center p-4 w-full rounded-md border border-gray-300 bg-white text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    >
+                        Fixed
+                    </button>
+                    <button
+                        type="button"
+                        class="items-center p-4 w-full rounded-md border border-gray-300 bg-white text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    >
+                        Choices
+                    </button>
+                </template>
+                <template
+                    class="border-y p-2 border-gray-200 focus-within:border-indigo-600"
+                >
+                    <Field
+                        name="Comment"
+                        as="textarea"
+                        class="block w-full resize-none border-transparent p-0 pb-2 focus:border-indigo-600 focus:rounded-lg focus:border-none focus:ring-0 sm:text-sm"
+                        placeholder="Menu description..."
+                    ></Field>
+                </template>
+            </Form>
         </div>
     </div>
 </template>
@@ -67,13 +63,8 @@ const props = defineProps({
 
 const store = useTooltipStore();
 
-// Close clickoutside
+// Close on click outside model
+
 const target = ref(null);
 onClickOutside(target, () => (store.toggle_tooltip = false));
-
-// close select component
-
-const toggleSelect = () => {
-    store.toggle_select = !store.toggle_select;
-};
 </script>
