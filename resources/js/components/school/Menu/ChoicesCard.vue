0926f9e42d135a6d1f8676bcde408d0e20ae25d5
chaiqou <lomtadzenikusha@gmail.com>
2023-02-08 17:56:34 +0400

    wip: choices and fixed cards templates

---
+++ resources/js/components/school/Menu/ChoicesCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div
+        class="fixed flex min-h-screen w-screen flex-col z-50 justify-center items-center bg-black/50"
+    >
+        <div
+            ref="target"
+            class="absolute md:right-1/3 xl:right-[45%] bg-white px-6 py-10 shadow-2xl max-w-lg rounded-lg w-full"
+        >
+            <template
+                class="flex flex-wrap justify-between border-b border-gray-200"
+            >
+                <h1 class="text-gray-700 mb-2 font-semibold">CHOICES CARD</h1>
+            </template>
+        </div>
+    </div>
+</template>
+
+<script setup></script>
