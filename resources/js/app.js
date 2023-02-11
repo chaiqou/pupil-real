abdc49e6b88d2227b999f58296c35421b3556dfd
chaiqou <lomtadzenikusha@gmail.com>
2023-02-12 02:32:51 +0400

    refactor: clean up app.js component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,11 @@
-import "./bootstrap";
+import "@/bootstrap";
 import "../css/app.css";
 import { createApp } from "vue/dist/vue.esm-bundler";
 import { createPinia } from "pinia";
 
-import * as Sentry from "@sentry/vue";
+import "@vuepic/vue-datepicker/dist/main.css";
+import "@/config/axios/index.js";
+import "@/config/vee-validate/index.js";
 
 import DashboardNavigation from "@/components/navigation/DashboardNavigation.vue";
 import DashboardNavigationMobile from "@/components/navigation/DashboardNavigationMobile.vue";
@@ -30,9 +32,6 @@ import AdminInvites from "@/components/admin/Invites/Invites.vue";
 import AdminInvitesHeader from "@/components/admin/Invites/InvitesHeader.vue";
 import ExtrasAndHolds from "@/components/lunch-managment/ExtrasAndHolds.vue";
 import LunchList from "@/components/lunch-managment/LunchList.vue";
-import "@vuepic/vue-datepicker/dist/main.css";
-import "@/config/axios/index.js";
-import "@/config/vee-validate/index.js";
 import AdminSchools from "@/components/admin/Schools/Schools.vue";
 import AdminMerchants from "@/components/admin/Merchants/Merchants.vue";
 import ParentLunchList from "@/components/parent/lunch-management/ParentLunchList.vue";
@@ -46,52 +45,67 @@ import ConfirmationModal from "@/components/lunch-managment/ConfirmationModal.vu
 import ParentLunchDetails from "@/components/parent/lunch-management/parentLunchDetails.vue";
 import SchoolTerminals from "@/components/school/Terminals/Terminals.vue";
 import SchoolTerminalsHeader from "@/components/school/Terminals/TerminalsHeader.vue";
-import ParentCalendar from "../js/components/parent/lunch-management/ParentCalendar.vue";
+import ParentCalendar from "@/components/parent/lunch-management/ParentCalendar.vue";
 import MenuCalendar from "@/components/Merchant/Menu-management/MenuCalendar.vue";
 import SchoolDashboard from "@/components/school/Dashboard/Dashboard.vue";
+
 const pinia = createPinia();
 const app = createApp({});
 
+// Navigation global components
 app.component("dashboard-navigation", DashboardNavigation);
 app.component("dashboard-navigation-mobile", DashboardNavigationMobile);
 app.component("navigation-menu-button", NavigationMenuButton);
-app.component("switch-account", SwitchAccount);
-app.component("school-transactions", SchoolTransactions);
+
+// Parent global components
 app.component("parent-transactions", ParentTransactions);
-app.component("school-students", SchoolStudents);
 app.component("parent-dashboard", ParentDashboard);
 app.component("parent-dashboard-transactions", ParentDashboardTransactions);
-app.component("calendar", Calendar);
-app.component("two-factor-auth-modal", TwoFactorAuthModal);
-app.component("change-password-modal", ChangePasswordModal);
 app.component("parent-students", ParentStudents);
-app.component("lunch-form", LunchForm);
-app.component("Datepicker", Datepicker);
+app.component("parent-students-mobile", ParentStudentsMobile);
+app.component("parent-lunch-list", ParentLunchList);
+app.component("parent-lunch-details", ParentLunchDetails);
+app.component("parent-calendar", ParentCalendar);
+
+// School global components
+app.component("school-terminals", SchoolTerminals);
+app.component("school-terminals-header", SchoolTerminalsHeader);
+app.component("school-transactions", SchoolTransactions);
+app.component("school-students", SchoolStudents);
 app.component("school-dashboard-students", SchoolDashboardStudents);
 app.component("school-dashboard-transactions", SchoolDashboardTransactions);
 app.component("school-invites", SchoolInvites);
 app.component("school-invites-header", SchoolInvitesHeader);
-app.component("admin-students", AdminStudents);
-app.component("admin-invites", AdminInvites);
-app.component("admin-invites-header", AdminInvitesHeader);
-app.component("extras-and-holds", ExtrasAndHolds);
-app.component("parent-students-mobile", ParentStudentsMobile);
-app.component("lunch-list", LunchList);
+app.component("school-dashboard", SchoolDashboard);
+
+// Admin global components
 app.component("admin-schools", AdminSchools);
 app.component("admin-merchants", AdminMerchants);
-app.component("parent-lunch-list", ParentLunchList);
 app.component("admin-merchants-header", AdminMerchantsHeader);
 app.component("admin-merchant-invites-header", AdminMerchantInvitesHeader);
 app.component("admin-merchant-invites", AdminMerchantInvites);
 app.component("admin-schools-header", AdminSchoolsHeader);
+app.component("admin-students", AdminStudents);
+app.component("admin-invites", AdminInvites);
+app.component("admin-invites-header", AdminInvitesHeader);
+
+// Calendar parts components
+app.component("calendar", Calendar);
+app.component("Datepicker", Datepicker);
+app.component("menu-calendar", MenuCalendar);
+app.component("extras-and-holds", ExtrasAndHolds);
+
+// Authentication components
+app.component("two-factor-auth-modal", TwoFactorAuthModal);
+app.component("change-password-modal", ChangePasswordModal);
+app.component("switch-account", SwitchAccount);
+
+// Lunch components
+app.component("lunch-form", LunchForm);
+app.component("lunch-list", LunchList);
 app.component("lunch-form-edit", LunchFormEdit);
 app.component("lunch-edit-page", LunchEditPage);
 app.component("confirmation-modal", ConfirmationModal);
-app.component("parent-lunch-details", ParentLunchDetails);
-app.component("school-terminals", SchoolTerminals);
-app.component("school-terminals-header", SchoolTerminalsHeader);
-app.component("parent-calendar", ParentCalendar);
-app.component("menu-calendar", MenuCalendar);
-app.component("school-dashboard", SchoolDashboard);
+
 app.use(pinia);
 app.mount("#app");
