86f1d97fbb5c71e3bd72b32f52b56a20c976985d
chaiqou <lomtadzenikusha@gmail.com>
2023-02-16 15:54:03 +0400

    style: change styles if save button is disabled

--- resources/js/components/Merchant/Menu-management/SaveAndDiscardButtons.vue
+++ resources/js/components/Merchant/Menu-management/SaveAndDiscardButtons.vue
@@ -11,6 +11,7 @@
       :disabled="isDisabled"
       type="button"
       class="basis-1/2 text-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+      :class="[isDisabled ? 'opacity-50 cursor-not-allowed' : '']"
     >
       Save
     </button>
