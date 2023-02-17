970425ee57f257a46a3d6650b73f56284592606d
chaiqou <lomtadzenikusha@gmail.com>
2023-02-17 16:01:43 +0400

    chore: small refactoring menu management card

--- resources/js/components/Merchant/Menu-management/MenuManagementCard.vue
+++ resources/js/components/Merchant/Menu-management/MenuManagementCard.vue
@@ -7,18 +7,18 @@
       class="absolute md:right-1/3 xl:right-[45%] bg-white px-6 py-10 shadow-2xl max-w-lg rounded-lg w-full"
     >
       <template class="flex flex-wrap justify-between border-b border-gray-200">
-        <h1 class="text-gray-700 mb-2 font-semibold">
+        <h2 class="text-gray-700 mb-2 font-semibold">
           {{ format(store.selectedDay, "yyyy MMMM dd") }}
-        </h1>
+        </h2>
       </template>
 
-      <div v-for="(lunch, index) in store.suitableLunch" :key="lunch.id">
+      <template v-for="lunch in store.suitableLunch" :key="lunch.id">
         <SingleLunchCard
           :lunch-id="lunch.id"
           :claimables="lunch.claimables"
           :name="lunch.title"
         />
-      </div>
+      </template>
     </div>
   </div>
 </template>
@@ -31,6 +31,8 @@ import { useMenuManagementStore } from "@/stores/useMenuManagementStore";
 import SingleLunchCard from "./SingleLunchCard.vue";
 
 const store = useMenuManagementStore();
+
+// Get suitable lunch for these days and return top await
 
 const getLunchData = async () => {
   try {
