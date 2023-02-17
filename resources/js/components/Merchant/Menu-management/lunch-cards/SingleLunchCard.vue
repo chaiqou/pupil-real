2502be0b748f82712c7e9293a40cf9b7e272725e
chaiqou <lomtadzenikusha@gmail.com>
2023-02-17 16:52:58 +0400

    refactor: remove duplicated button in singleLunchcard

--- resources/js/components/Merchant/Menu-management/lunch-cards/SingleLunchCard.vue
+++ resources/js/components/Merchant/Menu-management/lunch-cards/SingleLunchCard.vue
@@ -4,20 +4,15 @@
     <div
       class="flex flex-wrap space-y-4 w-full justify-between my-4 sm:space-y-0 sm:flex-nowrap sm:space-x-12"
     >
-      <button
-        type="button"
-        @click="onClickShowCard"
-        class="items-center p-4 w-full rounded-md border border-gray-300 bg-white text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-      >
-        Fixed
-      </button>
-      <button
-        type="button"
-        @click="onClickShowCard"
-        class="items-center p-4 w-full rounded-md border border-gray-300 bg-white text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-      >
-        Choices
-      </button>
+      <template v-for="option in options">
+        <button
+          type="button"
+          @click="onClickShowCard"
+          class="items-center p-4 w-full rounded-md border border-gray-300 bg-white text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        >
+          {{ option }}
+        </button>
+      </template>
     </div>
   </div>
 </template>
@@ -49,4 +44,6 @@ const onClickShowCard = () => {
   store.lunchName = props.name;
   store.lunchId = props.lunchId;
 };
+
+const options = ["Fixed", "Choices"];
 </script>
