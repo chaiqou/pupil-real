97157fd7e53f2a614c7ef5364a3ef39fae0e103f
chaiqou <lomtadzenikusha@gmail.com>
2023-02-21 19:11:26 +0400

    feat: retrieve correct data from backend menus and lunchs ( if user doesn't have it retrieves empty array )

--- app/Http/Controllers/Parent/Api/ParentMenuController.php
+++ app/Http/Controllers/Parent/Api/ParentMenuController.php
@@ -2,20 +2,24 @@
 
 namespace App\Http\Controllers\Parent\Api;
 
-use App\Http\Controllers\Controller;
 use App\Models\LunchMenu;
-use App\Models\PeriodicLunch;
 use Illuminate\Http\Request;
+use App\Models\PeriodicLunch;
+use Illuminate\Http\JsonResponse;
+use App\Http\Controllers\Controller;
 
 class ParentMenuController extends Controller
 {
-    public function menuRetrieve(Request $request)
+    public function menuRetrieve(Request $request): JsonResponse
     {
         $studentId = request('student_id');
 
-        $periodic_lunch = PeriodicLunch::where('student_id', $studentId)->pluck('lunch_id');
-        $menu = LunchMenu::whereIn('lunch_id', $periodic_lunch)->get();
+        $periodic_lunch = PeriodicLunch::where('student_id', $studentId)->get();
+        $periodic_lunch_ids = $periodic_lunch->pluck('id')->toArray();
+        $menu = LunchMenu::whereIn('lunch_id',  $periodic_lunch_ids)->get();
 
-        return $menu;
+        return response()->json(['menu' => $menu , 'lunch' => $periodic_lunch]);
     }
+
+
 }
