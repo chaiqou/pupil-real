b454f68244fa15dad024568a8b7d721e82c755a7
chaiqou <lomtadzenikusha@gmail.com>
2023-03-16 21:50:22 +0400

    feat: recreate parent menu options component instead of multiple render render only once now i have to submit form with outside button

--- resources/js/components/parent/Menus/ParentMenuOptions.vue
+++ resources/js/components/parent/Menus/ParentMenuOptions.vue
@@ -1,38 +1,42 @@
 <template>
-  <template class="flex flex-wrap justify-between">
+  <div v-for="menu in menus" :key="menu.name">
     <h2
       class="whitespace-wrap mb-2 w-64 overflow-hidden break-words font-semibold text-gray-700"
     >
-      {{ `${menu.date} - ${menu.menu_name}` }}
+      {{ `${menu.name} - ${menu.date}` }}
     </h2>
-  </template>
-  <Form @submit="onSubmit">
-    <div v-if="menu.menu_type === 'choices'">
-      <template v-for="menu in menu.menu_name" :key="menu">
-        <BaseRadio name="choices" :value="menu" />
-      </template>
-    </div>
-    <div v-if="menu.menu_type === 'fixed'">
-      <BaseRadio name="fixed" :value="menu.menu_name" />
-    </div>
-  </Form>
+
+    <Form @submit="onSubmitForm">
+      <div v-if="menu.menu_type === 'choices'">
+        <template v-for="menu in menu.menu_name" :key="menu">
+          <BaseRadio name="choices" :value="menu" />
+        </template>
+      </div>
+      <div v-if="menu.menu_type === 'fixed'">
+        <BaseRadio name="fixed" :value="menu.menu_name" />
+      </div>
+    </Form>
+  </div>
+
+  <Button text="Submit" class="ml-auto w-1/2" type="submit" />
 </template>
 
 <script setup>
 import BaseRadio from "@/components/Ui/form-components/BaseRadio.vue";
 import { Form } from "vee-validate";
 import { useMenuManagementStore } from "@/stores/useMenuManagementStore";
+import Button from "@/components/Ui/Button.vue";
 
 const store = useMenuManagementStore();
 
 const props = defineProps({
-  menu: {
+  menus: {
     type: Object,
     required: true,
   },
 });
 
-const onSubmit = function (values) {
+const onSubmitForm = function (values) {
   axios
     .post("/api/parent/choice-claims", {
       date: props.menu.date,
