22f6fa10b65e6a503f50869ac817334023334499
chaiqou <lomtadzenikusha@gmail.com>
2023-03-21 13:16:07 +0400

    fix: prevent update lunch claims automatically if menu type is not fixed

--- app/Jobs/UpdateFixedMenuClaims.php
+++ app/Jobs/UpdateFixedMenuClaims.php
@@ -34,23 +34,25 @@ class UpdateFixedMenuClaims implements ShouldQueue
         if ($periodic_lunch) {
             $claims_array = json_decode($periodic_lunch->claims, true);
 
-            // Loop through each date in the $claims_array and check if it matches the "day" value in $this->validated
-            foreach ($claims_array as $date => $claims) {
-                if ($date === $day) {
-                    // Loop through each claim for that date and check if the "name" value matches the "menu_type" value in $this->validated.
-                    foreach ($claims as $index => $claim) {
-                        foreach ($this->validated['menus'] as $menu_key => $menu) {
-                            if ($claim['name'] === $menu_key) {
-                                $claims_array[$date][$index]['menu'] = $menu;
-                                $claims_array[$date][$index]['menu_code'] = 0;
+            if ($claims_array[$day][0]['menu_type'] === 'Fixed') {
+                // Loop through each date in the $claims_array and check if it matches the "day" value in $this->validated
+                foreach ($claims_array as $date => $claims) {
+                    if ($date === $day) {
+                        // Loop through each claim for that date and check if the "name" value matches the "menu_type" value in $this->validated.
+                        foreach ($claims as $index => $claim) {
+                            foreach ($this->validated['menus'] as $menu_key => $menu) {
+                                if ($claim['name'] === $menu_key) {
+                                    $claims_array[$date][$index]['menu'] = $menu;
+                                    $claims_array[$date][$index]['menu_code'] = 0;
+                                }
                             }
                         }
                     }
                 }
-            }
 
-            $periodic_lunch->claims = json_encode($claims_array);
-            $periodic_lunch->save();
+                $periodic_lunch->claims = json_encode($claims_array);
+                $periodic_lunch->save();
+            }
         }
     }
 }
