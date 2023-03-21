e6880703320dd263986b6e0d3614fb1251daa2ea
chaiqou <lomtadzenikusha@gmail.com>
2023-03-21 12:59:19 +0400

    fix: avoid choices auto order

--- app/Http/Controllers/Parent/Api/OrderLunchController.php
+++ app/Http/Controllers/Parent/Api/OrderLunchController.php
@@ -90,25 +90,27 @@ class OrderLunchController extends Controller
                     $claims_array = json_decode($periodic_lunch->claims, true);
                     $menus_array = json_decode($lunch_menu['menus'], true);
 
-                    // Loop through each date in the $claims_array and check if it matches the "day" value in $validated
-                    foreach ($claims_array as $date => $claims) {
-                        if ($date === $day) {
-                            // Loop through each claim for that date and check if the "name" value matches the "menu_type" value in $this->validated.
-                            foreach ($claims as $index => $claim) {
-                                foreach ($menus_array as $menu_key => $menu) {
-                                    foreach ($menu as $menu_name) {
-                                        if ($claim['name'] === $menu_name['name']) {
-                                            $claims_array[$date][$index]['menu'] = $menu_name['menus'];
-                                            $claims_array[$date][$index]['menu_code'] = 0;
+                    if ($menus_array[$day][0]['menu_type'] === 'fixed') {
+                        // Loop through each date in the $claims_array and check if it matches the "day" value in $validated
+                        foreach ($claims_array as $date => $claims) {
+                            if ($date === $day) {
+                                // Loop through each claim for that date and check if the "name" value matches the "menu_type" value in $this->validated.
+                                foreach ($claims as $index => $claim) {
+                                    foreach ($menus_array as $menu_key => $menu) {
+                                        foreach ($menu as $menu_name) {
+                                            if ($claim['name'] === $menu_name['name']) {
+                                                $claims_array[$date][$index]['menu'] = $menu_name['menus'];
+                                                $claims_array[$date][$index]['menu_code'] = 0;
+                                            }
                                         }
                                     }
                                 }
                             }
                         }
-                    }
 
-                    $periodic_lunch->claims = json_encode($claims_array);
-                    $periodic_lunch->save();
+                        $periodic_lunch->claims = json_encode($claims_array);
+                        $periodic_lunch->save();
+                    }
                 }
             }
 
