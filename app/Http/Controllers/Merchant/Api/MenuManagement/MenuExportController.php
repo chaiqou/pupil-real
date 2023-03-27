cdffdfe1465f221e719bfcbb59fba6f82a1fb0da
chaiqou <lomtadzenikusha@gmail.com>
2023-03-27 15:28:41 +0400

    chore: find first and last day of week which comes from frontend

--- app/Http/Controllers/Merchant/Api/MenuManagement/MenuExportController.php
+++ app/Http/Controllers/Merchant/Api/MenuManagement/MenuExportController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Merchant\Api\MenuManagement;
 
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class MenuExportController extends Controller
 {
     public function exportMenu(Request $request)
     {
-        dd($request->all());
+        $firstDay = Carbon::parse($request[0]['date'])->startOfDay();
+        $lastDay = Carbon::parse($request[count($request->all()) - 1]['date'])->endOfDay();
     }
 }
