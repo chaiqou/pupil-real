886431d84682682623cecebd6347e51532e422cd
chaiqou <lomtadzenikusha@gmail.com>
2023-03-29 16:50:44 +0400

    refactor: create excel service and move lunch calculation logic inside service class

--- app/Http/Controllers/Merchant/Api/MenuManagement/MenuExportController.php
+++ app/Http/Controllers/Merchant/Api/MenuManagement/MenuExportController.php
@@ -3,37 +3,20 @@
 namespace App\Http\Controllers\Merchant\Api\MenuManagement;
 
 use App\Http\Controllers\Controller;
-use App\Models\Lunch;
-use Carbon\CarbonImmutable;
-use Carbon\CarbonPeriod;
+use App\Services\ExcelService;
 use Illuminate\Http\Request;
 
 class MenuExportController extends Controller
 {
+    protected $excelService;
+
+    public function __construct(ExcelService $excelService)
+    {
+        $this->excelService = $excelService;
+    }
+
     public function exportMenu(Request $request)
     {
-        $weekNumber = $request[0]['week'];
-
-        // Set the date to the first day of the given week number in the given year
-        $date = CarbonImmutable::now()->startOfYear()->startOfWeek()->addWeeks($weekNumber);
-
-        // Use the CarbonPeriod class to get all days in the week
-        $weekDays = [];
-        $period = CarbonPeriod::create($date->startOfDay(), $date->endOfWeek()->endOfDay());
-        foreach ($period as $day) {
-            $weekDays[] = $day->format('Y-m-d');
-        }
-
-        $startDate = $weekDays[0];
-        $endDate = end($weekDays);
-
-        // Find lunches which have a active range period between start and end date frontend data
-        $lunches = Lunch::all();
-        $filteredLunches = $lunches->filter(function ($lunch) use ($startDate, $endDate) {
-            return $lunch->active_range[0] <= $endDate && $startDate <= $lunch->active_range[1];
-        });
-        dd($filteredLunches->all());
-
-        return $weekDays;
+        $appropiateLunches = $this->excelService->findLunchesForExcelFile($request[0]['week']);
     }
 }
