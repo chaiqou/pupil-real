a4c8a076e174eef24041d885afe942d3ed26090e
chaiqou <lomtadzenikusha@gmail.com>
2023-04-03 18:12:31 +0400

    feat: instead of view return collection with headers in excel file

--- app/Exports/LunchOrdersExport.php
+++ app/Exports/LunchOrdersExport.php
@@ -2,11 +2,11 @@
 
 namespace App\Exports;
 
-use Illuminate\Contracts\View\View;
-use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class LunchOrdersExport implements FromView, ShouldAutoSize
+class LunchOrdersExport implements FromCollection, WithHeadings, ShouldAutoSize
 {
     private $lunches;
 
@@ -18,8 +18,27 @@ class LunchOrdersExport implements FromView, ShouldAutoSize
         $this->totalOrders = $totalOrders;
     }
 
-    public function view(): View
+    public function collection()
     {
-        return view('lunches-excel', ['lunches' => $this->lunches, 'totalOrders' => $this->totalOrders]);
+        $data = [];
+
+        foreach ($this->lunches as $lunch) {
+            $data[] = [
+                'Lunch ID' => $lunch->id,
+                'Lunch Name' => $lunch->title,
+                'Total Orders' => $this->totalOrders[$lunch->id] ?? 0,
+            ];
+        }
+
+        return collect($data);
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Lunch ID',
+            'Lunch Name',
+            'Total Orders',
+        ];
     }
 }
