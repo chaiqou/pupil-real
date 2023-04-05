e73e4ca1b27531205aba1e6eea16e7049a1af17c
chaiqou <lomtadzenikusha@gmail.com>
2023-04-05 13:50:33 +0400

    feat: Calculate menu_key as it is in periodic_lunch , based on this i have to count orders

--- app/Http/Controllers/Merchant/Api/MenuManagement/MenuExportController.php
+++ app/Http/Controllers/Merchant/Api/MenuManagement/MenuExportController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Merchant\Api\MenuManagement;
 use App\Exports\LunchOrdersExport;
 use App\Http\Controllers\Controller;
 use App\Models\Lunch;
+use App\Models\LunchMenu;
 use App\Services\ExcelService;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
@@ -24,6 +25,22 @@ class MenuExportController extends Controller
         $dayAndWeek = json_decode($dayAndWeekJson);
 
         $lunches = $this->excelService->findLunchesForExcelFile($dayAndWeek[0]->week);
+
+        // Calculate menu_key as it is in periodic_lunch , based on this i have to count orders
+
+        foreach ($lunches as $lunch) {
+            $lunchMenus = LunchMenu::where('lunch_id', $lunch->id)->get();
+
+            foreach ($lunchMenus as $lunchMenu) {
+                $menusArray = json_decode($lunchMenu->menus, true);
+
+                foreach ($menusArray as $date => $menuItems) {
+                    foreach ($menuItems as $index => $menuItem) {
+                        $menuKey = "{$lunchMenu['id']}-{$lunch['id']}-{$date}-{$menuItem['name']}-{$index}";
+                    }
+                }
+            }
+        }
 
         // convert array to collection
         $lunchesCollection = collect($lunches);
