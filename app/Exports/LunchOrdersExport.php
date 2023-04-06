8f2eca186d9891d0b8ea7d0da588235270a7bedc
chaiqou <lomtadzenikusha@gmail.com>
2023-04-06 15:08:21 +0400

    wip: send menus json with each lunch

--- app/Exports/LunchOrdersExport.php
+++ app/Exports/LunchOrdersExport.php
@@ -23,12 +23,20 @@ class LunchOrdersExport implements FromCollection, WithHeadings, ShouldAutoSize
         $data = [];
 
         foreach ($this->lunches as $lunch) {
+            $menusArray = []; // define an empty array before the inner loop
+            // loop over the lunch menus
+            foreach ($lunch['menus'] as $lunchMenu) {
+                $lunchMenusDecoded = json_decode($lunchMenu->menus, true);
+                $menusArray[] = $lunchMenusDecoded; // add the decoded menus to the array
+            }
+
             $totalOrders = isset($this->totalOrders[$lunch->id]) ? $this->totalOrders[$lunch->id] : 0;
 
             $data[] = [
                 'Lunch ID' => $lunch->id,
                 'Lunch Name' => $lunch->title,
                 'Total Orders' => $totalOrders ?: 'Not Ordered yet',
+                'Menus' => $menusArray, // assign the array to the 'Menus' key
             ];
         }
 
@@ -41,6 +49,7 @@ class LunchOrdersExport implements FromCollection, WithHeadings, ShouldAutoSize
             'Lunch ID',
             'Lunch Name',
             'Total Orders',
+            'Menus',
         ];
     }
 }
