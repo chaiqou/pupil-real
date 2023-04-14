1c0b052613421d56fc07e63e2bf9ca23f44241ad
chaiqou <lomtadzenikusha@gmail.com>
2023-04-15 01:19:04 +0400

    wip: update the origina menus json with the modified array add menu_count

--- app/Http/Controllers/Merchant/Api/MenuManagement/MenuExportController.php
+++ app/Http/Controllers/Merchant/Api/MenuManagement/MenuExportController.php
@@ -42,43 +42,31 @@ class MenuExportController extends Controller
                 // Each menu array's nested "Menus" json
                 $menuArray = json_decode($wholeMenu->menus, true);
 
-                foreach ($menuArray as $date => $menus) {
-                    // Get each menu seperatly
-                    foreach ($menus as $index => $menu) {
+                foreach ($menuArray as $date => &$menus) {
+                    // Get each menu separately
+                    foreach ($menus as $index => &$menu) {
                         // if menu type is choices
                         if (is_array($menu['menus'])) {
-                            foreach ($menu['menus'] as $subIndex => $subMenu) {
-                                // {menu_id}-{lunch_id}-{date}-{claimableName}-{indexKey}
-                                $choicesMenuKey = "{$wholeMenu['id']}-{$wholeMenu['lunch_id']}-{$date}-{$subMenu}-{$subIndex}";
+                            foreach ($menu['menus'] as $subIndex => &$subMenu) {
+                                // Construct choicesMenuKey
+                                $choicesMenuKey = "{$wholeMenu['id']}-{$wholeMenu['lunch_id']}-{$date}-{$menu['name']}-{$index}-{$subMenu['claimableName']}-{$subIndex}";
                                 $menuCount = PeriodicLunch::where('claims', 'LIKE', '%'.$choicesMenuKey.'%')->count();
+                                $subMenu['menu_count'] = $menuCount; // Add menu_count key with value to subMenu
                             }
                         } else {
+                            // Construct fixedMenuKey
                             $fixedMenuKey = "{$wholeMenu['id']}-{$wholeMenu['lunch_id']}-{$date}-{$menu['name']}-{$index}";
                             $menuCount = PeriodicLunch::where('claims', 'LIKE', '%'.$fixedMenuKey.'%')->count();
+                            $menu['menu_count'] = $menuCount; // Add menu_count key with value to menu
                         }
                     }
                 }
+
+                // Update the original "Menus" json with the modified array
+                $wholeMenu->menus = json_encode($menuArray);
+                $wholeMenu->save();
             }
         }
-
-        // $menuKey = "{$menuArray['id']}-{$menuArray['lunch_id']}-{$menuArray['date']}";
-
-        // // Calculate menu_key as it is in periodic_lunch , based on this i have to count orders
-        // foreach ($lunchesWithMenus  as $lunch) {
-        //     foreach ($lunch->menus as $lunchMenu) {
-        //         $menusArray = json_decode($lunchMenu->menus, true);
-
-        //         foreach ($menusArray as $date => &$menuItems) {
-        //             foreach ($menuItems as $index => &$menuItem) {
-        //                 $menuKey = "{$lunchMenu['id']}-{$lunch['id']}-{$date}-{$menuItem['name']}-{$index}";
-        //                 $menuCount = PeriodicLunch::where('claims', 'LIKE', '%'.$menuKey.'%')->count();
-        //                 $menuItem['menu_count'] = $menuCount; // add menu_count for each lunch option
-        //             }
-        //         }
-
-        //         $lunchMenu->menus = json_encode($menusArray); // update the modified menus back to the database
-        //     }
-        // }
 
         foreach ($lunches as $lunch) {
             if ($lunch->menus->count() == 0) {
