3054d9dbd614a69401a7357e78a408648e59ea35
pickle99 <jackrestler@gmail.com>
2023-04-18 11:45:28 +0400

    fix: transform InsightStatistic function calls from class to statis function call, remove required rule for middle name on onboardings

--- resources/js/components/admin/Schools/SchoolCreateModal.vue
+++ resources/js/components/admin/Schools/SchoolCreateModal.vue
@@ -187,7 +187,7 @@
                           autocomplete="state"
                           class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                         />
-                       
+
                       </div>
                     </div>
 
