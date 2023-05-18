392278434068fc077f5526b7a5341f27a2ac6e84
Leventdev <kazo.levente@gmail.com>
2023-05-18 21:01:02 +0200

    Update wording

--- resources/views/parent/dashboard.blade.php
+++ resources/views/parent/dashboard.blade.php
@@ -6,7 +6,7 @@
     <main class="flex-1">
         <div class="py-6">
             <div class="mx-auto  px-4 sm:px-6 md:px-8">
-                <h1 class="text-2xl font-semibold text-gray-900">{{$student->first_name}}{{auth()->user()->language === 'en' ? "'s Dashboard" : ' Irányítópult'}}</h1>
+                <h1 class="text-2xl font-semibold text-gray-900">{{$student->first_name}}{{auth()->user()->language === 'en' ? "'s Dashboard" : ' Irányítópultja'}}</h1>
                  <parent-dashboard :student-id="{{$studentId}}"></parent-dashboard>
             </div>
         </div>
