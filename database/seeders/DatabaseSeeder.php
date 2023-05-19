b3f689682a5eaa6dcd01ad000c97becd6234e5d0
chaiqou <lomtadzenikusha@gmail.com>
2023-05-19 21:40:28 +0400

    chore: Uncomment db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,19 +6,14 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
+    public function run(): void
     {
-//        $this->call(SchoolSeeder::class);
-//        $this->call(AdminSeeder::class);
+        $this->call(SchoolSeeder::class);
+        $this->call(AdminSeeder::class);
         $this->call(MerchantSeeder::class);
-//        $this->call(LunchSeeder::class);
-//        $this->call(ParentSeeder::class);
-//        $this->call(StudentSeeder::class);
-//        $this->call(TransactionSeeder::class);
+        $this->call(LunchSeeder::class);
+        $this->call(ParentSeeder::class);
+        $this->call(StudentSeeder::class);
+        $this->call(TransactionSeeder::class);
     }
 }
