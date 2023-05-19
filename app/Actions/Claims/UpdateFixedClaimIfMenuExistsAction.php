0b1cbf3b6267259e48c6df069d4fd66167e0d88b
chaiqou <lomtadzenikusha@gmail.com>
2023-05-19 15:12:39 +0400

    chore: update variable names based php style

--- app/Actions/Claims/UpdateFixedClaimIfMenuExistsAction.php
+++ app/Actions/Claims/UpdateFixedClaimIfMenuExistsAction.php
@@ -14,26 +14,26 @@ class UpdateFixedClaimIfMenuExistsAction
 
         foreach ($validated['claim_days'] as $date) {
             $day = Carbon::parse($date)->addDay()->format('Y-m-d');
-            $lunch_menu = LunchMenu::where('date', $day)->first();
-            $periodic_lunch = PeriodicLunch::where('claims', 'like', "%$day%")
+            $lunchMenu = LunchMenu::where('date', $day)->first();
+            $periodicLunch = PeriodicLunch::where('claims', 'like', "%$day%")
                 ->where('student_id', $validated['student_id'])
                 ->first();
 
-            if ($lunch_menu && $periodic_lunch) {
-                $claims_array = json_decode($periodic_lunch->claims, true);
-                $menus_array = json_decode($lunch_menu['menus'], true);
+            if ($lunchMenu && $periodicLunch) {
+                $claimsArray = json_decode($periodicLunch->claims, true);
+                $menusArray = json_decode($lunchMenu['menus'], true);
 
-                if ($menus_array[$day][0]['menu_type'] === 'fixed') {
-                    // Loop through each date in the $claims_array and check if it matches the "day" value in $validated
-                    foreach ($claims_array as $date => $claims) {
+                if ($menusArray[$day][0]['menu_type'] === 'fixed') {
+                    // Loop through each date in the $claimsArray and check if it matches the "day" value in $validated
+                    foreach ($claimsArray as $date => $claims) {
                         if ($date === $day) {
                             // Loop through each claim for that date and check if the "name" value matches the "menu_type" value in $this->validated.
                             foreach ($claims as $index => $claim) {
-                                foreach ($menus_array as $menu_key => $menu) {
+                                foreach ($menusArray as $menu_key => $menu) {
                                     foreach ($menu as $menu_name) {
                                         if ($claim['name'] === $menu_name['name']) {
-                                            $claims_array[$date][$index]['menu'] = $menu_name['menus'];
-                                            $claims_array[$date][$index]['menu_code'] = "{$lunch_menu['id']}-{$lunch_menu['lunch_id']}-{$date}-{$claim['name']}-{$index}";
+                                            $claimsArray[$date][$index]['menu'] = $menu_name['menus'];
+                                            $claimsArray[$date][$index]['menu_code'] = "{$lunchMenu['id']}-{$lunchMenu['lunch_id']}-{$date}-{$claim['name']}-{$index}";
                                         }
                                     }
                                 }
@@ -41,8 +41,8 @@ class UpdateFixedClaimIfMenuExistsAction
                         }
                     }
 
-                    $periodic_lunch->claims = json_encode($claims_array);
-                    $periodic_lunch->save();
+                    $periodicLunch->claims = json_encode($claimsArray);
+                    $periodicLunch->save();
                 }
             }
         }
--- app/Actions/Claims/UpdateFixedMenuClaimAction.php
+++ app/Actions/Claims/UpdateFixedMenuClaimAction.php
@@ -12,29 +12,29 @@ class UpdateFixedMenuClaimAction
     {
         // This logic automatically updates claims json in PeriodicLunch if menu for day already exists and menu type is fixed
         $day = Carbon::parse($validated['day'])->addDay()->format('Y-m-d');
-        $periodic_lunch = PeriodicLunch::where('claims', 'like', "%$day%")->first();
+        $periodicLunch = PeriodicLunch::where('claims', 'like', "%$day%")->first();
 
-        if ($periodic_lunch) {
-            $claims_array = json_decode($periodic_lunch->claims, true);
+        if ($periodicLunch) {
+            $claimsArray = json_decode($periodicLunch->claims, true);
 
             if ($validated['menu_type'] === 'Fixed') {
-                // Loop through each date in the $claims_array and check if it matches the "day" value in $validated
-                foreach ($claims_array as $date => $claims) {
+                // Loop through each date in the $claimsArray and check if it matches the "day" value in $validated
+                foreach ($claimsArray as $date => $claims) {
                     if ($date === $day) {
                         // Loop through each claim for that date and check if the "name" value matches the "menu_type" value in $validated.
                         foreach ($claims as $index => $claim) {
-                            foreach ($validated['menus'] as $menu_key => $menu) {
-                                if ($claim['name'] === $menu_key) {
-                                    $claims_array[$date][$index]['menu'] = $menu;
-                                    $claims_array[$date][$index]['menu_code'] = "{$menuId}-{$lunchId}-{$date}-{$claim['name']}-{$index}";
+                            foreach ($validated['menus'] as $menuKey => $menu) {
+                                if ($claim['name'] === $menuKey) {
+                                    $claimsArray[$date][$index]['menu'] = $menu;
+                                    $claimsArray[$date][$index]['menu_code'] = "{$menuId}-{$lunchId}-{$date}-{$claim['name']}-{$index}";
                                 }
                             }
                         }
                     }
                 }
 
-                $periodic_lunch->claims = json_encode($claims_array);
-                $periodic_lunch->save();
+                $periodicLunch->claims = json_encode($claimsArray);
+                $periodicLunch->save();
             }
         }
     }
