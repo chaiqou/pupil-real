4ddb1cad0c4f3171a3b62b9fbc048f3beda83966
chaiqou <lomtadzenikusha@gmail.com>
2023-05-22 15:37:23 +0400

    fix: remove 2FA condition checks from student actions

--- app/Actions/Auth/CheckMultipleStudentsAction.php
+++ app/Actions/Auth/CheckMultipleStudentsAction.php
@@ -6,6 +6,6 @@ class CheckMultipleStudentsAction
 {
     public static function execute($user): bool
     {
-        return $user->hasRole('parent') && $user->students->count() > 1 && session()->get('is_2fa_verified') === true;
+        return $user->hasRole('parent') && $user->students->count() > 1;
     }
 }
